gijskant/mcrl2-pmc

commit df6a8c3dad4a9f82fcdba1d278ab669708a072da

- Added exceptions to load and save functions of trace library. - Made some minor corrections to the documentation of the trace library. - Remove duplicate functionality from trace library. - Improved interface of LTS library a bit.
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@3979 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/trace/source/trace.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdexcept>
 #include "mcrl2/core/struct.h"
 #include "mcrl2/core/print.h"
 #include "mcrl2/trace.h"
@@ -96,24 +97,7 @@ void Trace::init()
 	// XXX set times[0] to gsMakeTime(0.0)?
 }
 
-Trace::Trace()
-{
-	init();
-}
-
-Trace::Trace(istream &is, TraceFormat tf)
-{
-	init();
-	load(is,tf);
-}
-
-Trace::Trace(string const& filename, TraceFormat tf)
-{
-	init();
-	load(filename,tf);
-}
-
-Trace::~Trace()
+void Trace::cleanup()
 {
 	ATunprotectArray((ATerm *) times);
 	ATunprotectArray((ATerm *) actions);
@@ -129,6 +113,41 @@ Trace::~Trace()
 	}
 }
 
+
+Trace::Trace()
+{
+	init();
+}
+
+Trace::Trace(istream &is, TraceFormat tf)
+{
+	init();
+	try
+	{
+		load(is,tf);
+	} catch ( ... ) {
+		cleanup();
+		throw;
+	}
+}
+
+Trace::Trace(string const& filename, TraceFormat tf)
+{
+	init();
+	try
+	{
+		load(filename,tf);
+	} catch ( ... ) {
+		cleanup();
+		throw;
+	}
+}
+
+Trace::~Trace()
+{
+	cleanup();
+}
+
 void Trace::resetPosition()
 {
 	pos = 0;
@@ -136,7 +155,7 @@ void Trace::resetPosition()
 
 void Trace::setPosition(unsigned int pos)
 {
-	if ( pos < len )
+	if ( pos <= len )
 	{
 		this->pos = pos;
 	}
@@ -164,12 +183,14 @@ ATermAppl Trace::currentTime()
 
 ATermAppl Trace::nextAction()
 {
+	ATermAppl act = actions[pos];
+	
 	if ( pos < len )
 	{
-		return actions[pos++];
-	} else {
-		return NULL;
+		pos++;
 	}
+
+	return act;
 }
 
 void Trace::truncate()
@@ -226,34 +247,16 @@ bool Trace::canSetState()
 }
 
 
-ATermAppl Trace::getAction()
-{
-	ATermAppl act = actions[pos];
-	
-	if ( pos < len )
-	{
-		pos++;
-	}
-
-	return act;
-}
-
-ATermAppl Trace::getState()
-{
-	return states[pos];
-}
-
-ATermAppl Trace::getTime()
-{
-	return times[pos];
-}
-
 TraceFormat Trace::detectFormat(istream &is)
 {
 	char buf[TRACE_MCRL2_MARKER_SIZE];
 	TraceFormat fmt = tfPlain;
 
 	is.read(buf,TRACE_MCRL2_MARKER_SIZE);
+        if ( !is.good() )
+        {
+          throw runtime_error("could not read from stream");
+        }
 
 	if ( (is.gcount() == TRACE_MCRL2_MARKER_SIZE) && !strncmp(buf,TRACE_MCRL2_MARKER,TRACE_MCRL2_MARKER_SIZE) )
 	{
@@ -261,6 +264,10 @@ TraceFormat Trace::detectFormat(istream &is)
 	}
 
 	is.seekg(-is.gcount(),ios::cur);
+        if ( !is.good() )
+        {
+          throw runtime_error("could set position in stream");
+        }
 
 	return fmt;
 }
@@ -274,13 +281,30 @@ ATerm readATerm(istream &is)
 
 	while ( !is.eof() )
 	{
-		buf = (char *) realloc(buf,buf_size*sizeof(char));
+		char *new_buf = (char *) realloc(buf,buf_size*sizeof(char));
+                if ( new_buf == NULL )
+                {
+                  free(buf);
+                  throw runtime_error("not enough memory to read ATerm");
+                }
+                buf = new_buf;
+
 		is.read(buf+len,buf_size-len);
+                if ( !is.good() )
+                {
+                  free(buf);
+                  throw runtime_error("could not read ATerm from stream");
+                }
+
 		len+=is.gcount();
 		buf_size = buf_size * 2;
 	}
 
 	ATerm t = ATreadFromBinaryString((unsigned char *) buf,len);
+        if ( t == NULL )
+        {
+          throw runtime_error("failed to read ATerm from stream");
+        }
 
 	free(buf);
 
@@ -291,13 +315,16 @@ void Trace::loadMcrl2(istream &is)
 {
 	char buf[TRACE_MCRL2_MARKER_SIZE+TRACE_MCRL2_VERSION_SIZE];
 	is.read(buf,TRACE_MCRL2_MARKER_SIZE+TRACE_MCRL2_VERSION_SIZE);
-
-	assert(!strncmp(buf,TRACE_MCRL2_MARKER,TRACE_MCRL2_MARKER_SIZE));
+        if ( !is.good() || strncmp(buf,TRACE_MCRL2_MARKER,TRACE_MCRL2_MARKER_SIZE) )
+        {
+          throw runtime_error("stream does not contain an mCRL2 trace");
+        }
 
 	resetPosition();
 	truncate();
 
-	ATermList trace = (ATermList) readATerm(is); //XXX
+	ATermList trace = (ATermList) readATerm(is);
+        assert( ATgetType(trace) == AT_LIST );
 
 	for (; !ATisEmpty(trace); trace=ATgetNext(trace))
 	{
@@ -332,7 +359,11 @@ void Trace::loadPlain(istream &is)
 	while ( !is.eof() )
 	{
 		is.getline(buf,MAX_LINE_SIZE);
-		if  ( is.gcount() > 0 )
+                if ( !is.good() ) 
+                {
+                  throw runtime_error("error while reading from stream");
+                }
+		if ( is.gcount() > 0 )
 		{
 			// XXX need to parse trace
 			addAction(ATmakeAppl0(ATmakeAFun(buf,0,ATfalse)));
@@ -344,38 +375,55 @@ void Trace::loadPlain(istream &is)
 
 void Trace::load(istream &is, TraceFormat tf)
 {
-	if ( tf == tfUnknown )
+	try
 	{
-		tf = detectFormat(is);
-	}
 
-	switch ( tf )
+		if ( tf == tfUnknown )
+		{
+			tf = detectFormat(is);
+		}
+
+		switch ( tf )
+		{
+			case tfMcrl2:
+				loadMcrl2(is);
+				break;
+			case tfPlain:
+				loadPlain(is);
+				break;
+			default:
+				break;
+		}
+
+	} catch ( runtime_error err )
 	{
-		case tfMcrl2:
-			loadMcrl2(is);
-			break;
-		case tfPlain:
-			loadPlain(is);
-			break;
-		default:
-			break;
+		string s;
+		s = "error loading trace (";
+		s += err.what();
+		s += ")";
+		throw runtime_error(s);
 	}
 }
 
-bool Trace::load(string const& filename, TraceFormat tf)
+void Trace::load(string const& filename, TraceFormat tf)
 {
 	ifstream is(filename.c_str(),ifstream::binary|ifstream::in);
 
 	if ( !is.is_open() )
 	{
-		return false;
+		throw runtime_error("error loading trace (could not open file)");
 	}
 
-	load(is, tf);
+	try
+	{
+		load(is, tf);
+	} catch (...)
+	{
+		is.close();
+		throw;
+	}
 
 	is.close();
-
-	return true;
 }
 
 void Trace::saveMcrl2(ostream &os)
@@ -402,11 +450,22 @@ void Trace::saveMcrl2(ostream &os)
 		}
 	}
 
+	// write marker
 	os << TRACE_MCRL2_MARKER;
 	os.write(TRACE_MCRL2_VERSION,TRACE_MCRL2_VERSION_SIZE);
+	if ( os.bad() )
+	{
+		throw runtime_error("could not write to stream");
+	}
+
+	// write trace
 	int len;
-	const char *bs = (const char *) ATwriteToBinaryString((ATerm) trace,&len); //XXX
+	const char *bs = (const char *) ATwriteToBinaryString((ATerm) trace,&len); //XXX no error handling?
 	os.write(bs,len);
+	if ( os.bad() )
+	{
+		throw runtime_error("could not write to stream");
+	}
 }
 
 void Trace::savePlain(ostream &os)
@@ -420,35 +479,54 @@ void Trace::savePlain(ostream &os)
 			os << ATwriteToString((ATerm) actions[i]);
 		}
 		os << endl;
+		if ( os.bad() )
+		{
+			throw runtime_error("could not write to stream");
+		}
 	}
 }
 
 void Trace::save(ostream &os, TraceFormat tf)
 {
-	switch ( tf )
+	try
 	{
-		case tfMcrl2:
-			saveMcrl2(os);
-			break;
-		case tfPlain:
-			savePlain(os);
-			break;
-		default:
-			break;
+		switch ( tf )
+		{
+			case tfMcrl2:
+				saveMcrl2(os);
+				break;
+			case tfPlain:
+				savePlain(os);
+				break;
+			default:
+				break;
+		}
+	} catch (runtime_error err)
+	{
+		string s;
+		s = "error saving trace (";
+		s += err.what();
+		s += ")";
+		throw runtime_error(s);
 	}
 }
 
-bool Trace::save(string const& filename, TraceFormat tf)
+void Trace::save(string const& filename, TraceFormat tf)
 {
 	ofstream os(filename.c_str(),ofstream::binary|ofstream::out|ofstream::trunc);
 	if ( !os.is_open() )
 	{
-		return false;
+		throw runtime_error("error saving trace (could not open file)");
 	}
 
-	save(os, tf);
+	try
+	{
+		save(os, tf);
+	} catch (...)
+	{
+	        os.close();
+		throw;
+	}
 
 	os.close();
-
-	return true;
 }

libraries/lts/include/mcrl2/lts/detail/liblts_private.h
@@ -84,8 +84,8 @@
       lts_type detect_type(std::string const& filename);
       lts_type detect_type(std::istream &is);
 
-      void init(lts_type type = lts_mcrl2, bool state_info = true, bool label_info = true);
-      void clear(lts_type type = lts_mcrl2, bool state_info = true, bool label_info = true);
+      void init(bool state_info = true, bool label_info = true);
+      void clear(bool state_info = true, bool label_info = true);
 
       unsigned int p_add_state(ATerm value = NULL);
       unsigned int p_add_label(ATerm value = NULL, bool is_tau = false);

libraries/lts/include/mcrl2/lts/liblts.h
@@ -352,12 +352,11 @@ namespace lts
 
     public:
       /** Creates an empty LTS.
-       * \param[in] type The LTS format.
        * \param[in] state_info Indicates whether state parameter information
        * will be present.
        * \param[in] label_info Indicates whether label information will be
        * present. */
-      lts(lts_type type = lts_mcrl2, bool state_info = true, bool label_info = true);
+      lts(bool state_info = true, bool label_info = true);
       /** Creates an LTS and reads its data from a file.
        * \param[in] filename The name of the file from which the data will be
        * read.
@@ -375,12 +374,11 @@ namespace lts
       ~lts();
 
       /** Clears the LTS data. 
-       * \param[in] type The new LTS format.
        * \param[in] state_info Indicates whether state parameter information
        * will be present.
        * \param[in] label_info Indicates whether label information will be
        * present. */
-      void reset(lts_type type = lts_mcrl2, bool state_info = true, bool label_info = true);
+      void reset(bool state_info = true, bool label_info = true);
 
       /** Reads LTS data from a file. This is not supported for Dot files.
        * \param[in] filename The name of the file from which data will be read.
@@ -530,7 +528,9 @@ namespace lts
        * \param[in] creator The creator string.*/
       void set_creator(std::string creator);
       
-      /** Gets the format of this LTS.
+      /** Gets the type of the file of the last succesful call to load. Note
+       * that this does not mean that the information currently in the object
+       * is still in this format.
        * \return The format of this LTS. */
       lts_type get_type();
 

libraries/lts/source/bsimdata.cpp
@@ -278,7 +278,7 @@ static ATerm apply_hiding(ATerm act)
       }
       if ( find(tau_actions->begin(),tau_actions->end(),s) != tau_actions->end() )
       {
-        act = (ATerm) ATmakeAppl0(ATmakeAFun("tau",0,ATtrue));
+        act = label_name[label_tau];
       }
     }
   }
@@ -323,17 +323,7 @@ int ReadData(lts &l)
    nlabel = l.num_labels() + 1; // +1 for tau label
    AllocData();
    label_tau = nlabel-1;
-   if ( l.get_type() == lts_mcrl2 )
-   {
-     label_name[label_tau] = (ATerm) gsMakeMultAct(ATmakeList0());
-#ifdef MCRL2_BCG
-   } else if ( l.get_type() == lts_bcg )
-   {
-     label_name[label_tau] = (ATerm) ATmakeAppl0(ATmakeAFun("i",0,ATtrue));
-#endif
-   } else {
-     label_name[label_tau] = (ATerm) ATmakeAppl0(ATmakeAFun("tau",0,ATtrue));
-   }
+   label_name[label_tau] = (ATerm) gsMakeMultAct(ATmakeList0());
    transition_iterator i(&l);
    for (; i.more(); ++i) 
       {
@@ -349,22 +339,17 @@ static void pp_lts(lts &l)
 {
   for (unsigned int i=0; i<l.num_labels(); i++)
   {
-    l.set_label(i,(ATerm) ATmakeAppl0(ATmakeAFun(PrintPart_CXX(l.label_value(i),ppDefault).c_str(),0,ATtrue)),l.is_tau(i));
+    if ( ATisAppl(l.label_value(i)) && gsIsMultAct((ATermAppl) l.label_value(i)) )
+    {
+      l.set_label(i,(ATerm) ATmakeAppl0(ATmakeAFun(PrintPart_CXX(l.label_value(i),ppDefault).c_str(),0,ATtrue)),l.is_tau(i));
+    }
   }
 }
 
 void ReadCompareData(lts &l1, int *init1, lts &l2, int *init2) 
    {
-     if ( l1.get_type() != l2.get_type() )
-     {
-       if ( l1.get_type() == lts_mcrl2 )
-       {
-         pp_lts(l1);
-       } else if ( l2.get_type() == lts_mcrl2 )
-       {
-         pp_lts(l2);
-       }
-     }
+   pp_lts(l1);
+   pp_lts(l2);
 
    second_lts_states_offset = l1.num_states();
    int offset = second_lts_states_offset;
@@ -373,17 +358,7 @@ void ReadCompareData(lts &l1, int *init1, lts &l2, int *init2)
    nlabel = l1.num_labels()+l2.num_labels()+1; 
    AllocData();
    label_tau = nlabel-1;
-   if ( l1.get_type() == lts_mcrl2 )
-   {
-     label_name[label_tau] = (ATerm) gsMakeMultAct(ATmakeList0());
-#ifdef MCRL2_BCG
-   } else if ( l1.get_type() == lts_bcg )
-   {
-     label_name[label_tau] = (ATerm) ATmakeAppl0(ATmakeAFun("i",0,ATtrue));
-#endif
-   } else {
-     label_name[label_tau] = (ATerm) ATmakeAppl0(ATmakeAFun("tau",0,ATtrue));
-   }
+   label_name[label_tau] = (ATerm) gsMakeMultAct(ATmakeList0());
 
    for (transition_iterator i(&l1); i.more(); ++i)
       {
@@ -619,7 +594,7 @@ int ReturnEquivalenceClasses(int initState, ATbool
 int WriteData(lts &l, int initState, bool omit_tauloops)
     {
     int newState = ReturnEquivalenceClasses(initState, omit_tauloops?ATtrue:ATfalse);
-    l.reset(l.get_type(),false);
+    l.reset(false);
     WriteTransitions(l);
     l.set_initial_state(newState);
     return 1;     

libraries/lts/source/liblts.cpp
@@ -126,9 +126,9 @@ p_lts::p_lts(lts *l)
   lts_object = l;
 }
 
-lts::lts(lts_type type, bool state_info, bool label_info) : p_lts(this)
+lts::lts(bool state_info, bool label_info) : p_lts(this)
 {
-  init(type,state_info,label_info);
+  init(state_info,label_info);
 }
 
 lts::lts(string &filename, lts_type type) : p_lts(this)
@@ -160,12 +160,12 @@ lts::~lts()
   free(transitions);
 }
 
-void lts::reset(lts_type type, bool state_info, bool label_info)
+void lts::reset(bool state_info, bool label_info)
 {
-  clear(type,state_info,label_info);
+  clear(state_info,label_info);
 }
 
-void p_lts::init(lts_type type, bool state_info, bool label_info)
+void p_lts::init(bool state_info, bool label_info)
 {
   states_size = 0;
   nstates = 0;
@@ -180,12 +180,12 @@ void p_lts::init(lts_type type, bool state_info, bool label_info)
   ntransitions = 0;
   transitions = NULL;
   
-  this->type = type;
+  this->type = lts_none;
   this->state_info = state_info;
   this->label_info = label_info;
 }
 
-void p_lts::clear(lts_type type, bool state_info, bool label_info)
+void p_lts::clear(bool state_info, bool label_info)
 {
   if ( state_values != NULL )
   {
@@ -200,7 +200,7 @@ void p_lts::clear(lts_type type, bool state_info, bool label_info)
   }
   free(transitions);
 
-  init(type,state_info,label_info);
+  init(state_info,label_info);
 }
 
 lts_type p_lts::detect_type(string const& filename)

libraries/lts/source/liblts_bcg.cpp
@@ -115,9 +115,24 @@ bool p_lts::write_to_bcg(string const& filename)
 
   char *buf = NULL;
   unsigned int buf_size = 0;
+  bool warn_non_i = true;
+  bool warn_i = true;
   for (unsigned int i=0; i<ntransitions; i++)
   {
     string label_str = p_label_value_str(transitions[i].label);
+    if ( taus[transitions[i].label] )
+    {
+      if ( warn_non_i && (label_str != "i") )
+      {
+        gsWarningMsg("LTS contains silent steps that are not labelled 'i'; saving as BCG means they are replaced by 'i'.\n");
+        warn_non_i = false;
+      }
+      label_str = "i";
+    } else if ( warn_i && (label_str == "i") )
+    {
+      gsWarningMsg("LTS contains label 'i' without being marked as silent step; saving as BCG means it is assumed to be a silent step.\n");
+      warn_i = false;
+    }
     if ( label_str.size() > buf_size )
     {
       if ( buf_size == 0 )

libraries/trace/include/mcrl2/trace.h
@@ -21,10 +21,10 @@
 //
 /// \brief Formats in which traces can be saved on disk
 /// There are several formats for traces.
-/// The tfMcrl2 format saves a trace as an mcrl2 term in ATerm internal.
+/// The tfMcrl2 format saves a trace as an mCRL2 term in ATerm internal format.
 /// This is a compact but unreadable format.
 /// The tfPlain format is an ascii representation of the trace, which is
-/// human readable but only contains the actions and not time or state information. 
+/// human readable but only contains the actions and no time or state information. 
 /// tfUnknown is only used to read traces, when it is
 /// not known what the format is. In this case it is determined based
 /// on the format of the input file.
@@ -35,7 +35,7 @@ enum TraceFormat { tfMcrl2, tfPlain, tfUnknown };
 /// tag as a positive real number. Between two actions, before the first action and after the last
 /// action there can be a state. In the current version of the trace library an action, a state and
 /// a time tag are arbitrary expressions of sort AtermAppl. It is expected that this will change
-/// in the near future to match the data types used in the lps library.
+/// in the near future to match the data types used in the LPS library.
 /// 
 /// An important property of a state is its current position. All operations on a state
 /// operate with respect to its current position. A trace can be traversed by increasing
@@ -52,25 +52,21 @@ class Trace
 	public:
 /// \brief Default constructor for an empty trace.
 /// \details This is the default constructor for an empty trace. The current position
-/// and length of trace are set to 0. The initial state, its outgoing action
-/// and time are set to NULL. Initially space is reserved for 64 actions,
-/// states and time tags which is increased by need.
+/// and length of trace are set to 0. The initial state and time are set to NULL.
 		Trace();
 /// \brief Construct the trace in the basis of an input stream.
 /// \details A trace is read from the input stream. If the format is tfUnknown it
-/// is automatically determined what the format of the trace is. At
-/// termination the current position of the trace is at the end of the trace,
-/// after the last action.
+/// is automatically determined what the format of the trace is.
 /// \param[in] is The input stream from which the trace is read.
 /// \param[in] tf The format in which the trace was stored. Default: '''tfUnknown'''.
+/// \exception std::runtime_error message in case of failure
 		Trace(std::istream &is, TraceFormat tf = tfUnknown);
 /// \brief Construct the trace on the basis of an input file.
 /// \details A trace is read from the input file. If the format is tfUnknown it
-/// is automatically determined what the format of the trace is. At
-/// termination the current position of the trace is at the end of the trace,
-/// after the last action.
+/// is automatically determined what the format of the trace is.
 /// \param[in] filename The name of the file from which the trace is read.
 /// \param[in] tf The format in which the trace was stored. Default: '''tfUnknown'''.
+/// \exception std::runtime_error message in case of failure
 		Trace(std::string const& filename, TraceFormat tf = tfUnknown);
 /// \brief Destructor for the trace.
 /// \details Destructor for the trace. It frees all the associated memory of the trace.
@@ -83,7 +79,7 @@ class Trace
 
 /// \brief Set the current position after the pos'th action of the trace
 /// \details Set the current position after the pos'th action of the trace. The
-/// initial position corresponds to pos=0. If pos is larger or equal to
+/// initial position corresponds to pos=0. If pos is larger than
 /// the length of the trace, no new position is set.
 /// \param[in] pos The new position in the trace.
 		void setPosition(unsigned int pos);
@@ -105,22 +101,24 @@ class Trace
 /// the current position NULL is returned.
 /// \return The state at the current position of the trace.
 		ATermAppl currentState();
-/// \brief Get the outgoing action from the current position in the trace
+/// \brief Get the outgoing action from the current position in the trace and
+//  move to the next position
 /// \details This routine returns the action at the current position of the
-/// trace. This actions is always defined.
+/// trace and moves to the next position in the trace. When the current position
+/// is at the end of the trace, nothing happens and NULL is returned.
 /// \return An ATermAppl representing the action at the current position of the
-/// trace.
+/// trace. This is NULL when at the end of the trace.
 		ATermAppl nextAction();
-/// \brief Get the time of the current action from the current state in the trace
-/// \details Get the time of the current action from the current state in the trace. If 
-/// the current action is untimed, NULL is returned.
+/// \brief Get the time of the current state in the trace
+/// \details Get the time of the current state in the trace. This is the time at which
+/// the last action occurred (if any).
 /// \return An ATermAppl representing the current time, or NULL if the time is not defined.
 		ATermAppl currentTime();
 
-/// \brief Truncates the trace from the current position.
-/// \details This function removes the action at the current position and all
+/// \brief Truncates the trace at the current position.
+/// \details This function removes the mext action at the current position and all
 /// subsequent actions, times and states. The state and the time at the current
-/// position remain untouched. The length is set to the current position.
+/// position remain untouched.
 		void truncate();
 
 /// \brief
@@ -142,31 +140,13 @@ class Trace
 		bool canSetState();
 
 
-/// \brief Not documented. Use nextAction.
-/// \details
-/// \param [in]
-/// \return
-		ATermAppl getAction();
-
-/// \brief Not documented.  Use currentState.
-/// \details
-/// \param [in]
-/// \return
-		ATermAppl getState();
-
-/// \brief Not documented. Use currentTime.
-/// \details
-/// \param [in]
-/// \return
-		ATermAppl getTime();
-
-
 /// \brief Replace the trace with the content of the stream.
 /// \details The trace is replaced with the trace in the stream.
 /// If a problem occurs while reading the stream, a core dump occurs.
 /// If the format is tfPlain the trace can only consist of actions.
 /// \param [in] is The stream from which the trace is read.
 /// \param [in] tf The expected format of the trace in the stream (default: tfUnknown).
+/// \exception std::runtime_error message in case of failure
 		void load(std::istream &is, TraceFormat tf = tfUnknown);
 
 /// \brief Replace the trace with the trace in the file.
@@ -174,10 +154,9 @@ class Trace
 /// If the format is tfPlain the trace can only consist of actions.
 /// \param [in] filename The name of the file from which the trace is read.
 /// \param [in] tf The expected format of the trace in the stream (default: tfUnknown).
-/// \retval true The string was successfully read from the file.
-/// \retval false A problem occurred when reading the string from the file.
+/// \exception std::runtime_error message in case of failure
 
-		bool load(std::string const& filename, TraceFormat tf = tfUnknown);
+		void load(std::string const& filename, TraceFormat tf = tfUnknown);
 
 /// \brief Output the trace into the indicated stream.
 /// \details Output the trace into the indicated stream. 
@@ -185,6 +164,7 @@ class Trace
 /// \param [in] os The stream to which the trace is written.
 /// \param [in] tf The format used to represent the trace in the stream. If 
 /// the format is tfPlain only actions are written. Default: tfMcrl2.
+/// \exception std::runtime_error message in case of failure
 
 		void save(std::ostream &os, TraceFormat tf = tfMcrl2);
 
@@ -193,10 +173,9 @@ class Trace
 /// \param [in] filename The name of the file that is written.
 /// \param [in] tf The format used to represent the trace in the stream. If 
 /// the format is tfPlain only actions are written. Default: tfMcrl2.
-/// \return A boolean is returned to indicate whether writing of the trace
-/// was successful.
+/// \exception std::runtime_error message in case of failure
 
-		bool save(std::string const& filename, TraceFormat tf = tfMcrl2);
+		void save(std::string const& filename, TraceFormat tf = tfMcrl2);
 
 	private:
 		ATermAppl *states;
@@ -207,6 +186,7 @@ class Trace
 		unsigned int pos;
 
 		void init();
+                void cleanup();
 
 		TraceFormat detectFormat(std::istream &is);
 		void loadMcrl2(std::istream &is);

tools/lps2lts/exploration.cpp
@@ -264,7 +264,15 @@ static bool savetrace(string const &info, ATerm state, ATermTable backpointers, 
     trace.setState(nstate->makeStateVector(ATgetFirst(e)));
   }
 
-  return trace.save(lgopts->generate_filename_for_trace(info, "trc"));
+  try
+  {
+    trace.save(lgopts->generate_filename_for_trace(info, "trc"));
+  } catch ( ... )
+  {
+    return false;
+  }
+
+  return true;
 }
 
 std::string lts_generation_options::generate_trace_file_name(std::string const& info, std::string const& extension) {

commit a342122373f08d284c374c4c364db7c63590a678

Fixed bug introduced in r3979; loading of traces would always fail.
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@4027 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/trace/source/trace.cpp
@@ -252,10 +252,11 @@ TraceFormat Trace::detectFormat(istream &is)
 	TraceFormat fmt = tfPlain;
 
 	is.read(buf,TRACE_MCRL2_MARKER_SIZE);
-        if ( !is.good() )
+        if ( is.bad() )
         {
           throw runtime_error("could not read from stream");
         }
+        is.clear();
 
 	if ( (is.gcount() == TRACE_MCRL2_MARKER_SIZE) && !strncmp(buf,TRACE_MCRL2_MARKER,TRACE_MCRL2_MARKER_SIZE) )
 	{
@@ -263,7 +264,7 @@ TraceFormat Trace::detectFormat(istream &is)
 	}
 
 	is.seekg(-is.gcount(),ios::cur);
-        if ( !is.good() )
+        if ( is.fail() )
         {
           throw runtime_error("could set position in stream");
         }
@@ -289,7 +290,7 @@ ATerm readATerm(istream &is)
                 buf = new_buf;
 
 		is.read(buf+len,buf_size-len);
-                if ( !is.good() )
+                if ( is.bad() )
                 {
                   free(buf);
                   throw runtime_error("could not read ATerm from stream");
@@ -298,6 +299,7 @@ ATerm readATerm(istream &is)
 		len+=is.gcount();
 		buf_size = buf_size * 2;
 	}
+        is.clear();
 
 	ATerm t = ATreadFromBinaryString((unsigned char *) buf,len);
         if ( t == NULL )
@@ -314,10 +316,11 @@ void Trace::loadMcrl2(istream &is)
 {
 	char buf[TRACE_MCRL2_MARKER_SIZE+TRACE_MCRL2_VERSION_SIZE];
 	is.read(buf,TRACE_MCRL2_MARKER_SIZE+TRACE_MCRL2_VERSION_SIZE);
-        if ( !is.good() || strncmp(buf,TRACE_MCRL2_MARKER,TRACE_MCRL2_MARKER_SIZE) )
+        if ( is.bad() || strncmp(buf,TRACE_MCRL2_MARKER,TRACE_MCRL2_MARKER_SIZE) )
         {
           throw runtime_error("stream does not contain an mCRL2 trace");
         }
+        is.clear();
 
 	resetPosition();
 	truncate();
@@ -358,7 +361,7 @@ void Trace::loadPlain(istream &is)
 	while ( !is.eof() )
 	{
 		is.getline(buf,MAX_LINE_SIZE);
-		if ( !is.good() ) 
+		if ( is.bad() ) 
 		{
 			throw runtime_error("error while reading from stream");
 		}
@@ -373,6 +376,7 @@ void Trace::loadPlain(istream &is)
 			addAction(ATmakeAppl0(ATmakeAFun(buf,0,ATfalse)));
 		}
 	}
+        is.clear();
 
 	resetPosition();
 }