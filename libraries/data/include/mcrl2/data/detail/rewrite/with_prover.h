gijskant/mcrl2-pmc

commit 84da6ceae7887c13dbe147194b99e0cf7ac3c5f3

- Fixed defect #444; rewrite rules of the form x -> t were mistakenly   allowed. - Implemented enhancement #103; compiling rewriters are no longer available   (in code and help messages) when they are not supported. As part of this I   have introduced defines MCRL2_INNERC_AVAILABLE and MCRL2_JITTYC_AVAILABLE   that, when defined, indicate that the compiling innermost and JITty   rewriters, respectively, are available. - Implemented enhancement #151; added some auxiliary function to the   rewriters. Note that the old functionality of setSubstitution() and   getSubstitution() are now available via setSubstitutionInternal() and   getSubstitutionInternal(), respectively. The setSubstitution() and   getSubstitution() now work with mCRL2 data expressions (instead of terms   in the internal rewriter format).
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5272 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/data/include/mcrl2/data/detail/rewrite/with_prover.h
@@ -34,10 +34,15 @@ class RewriterProver: public Rewriter
 		bool addRewriteRule(ATermAppl Rule);
 		bool removeRewriteRule(ATermAppl Rule);
 		
-		void setSubstitution(ATermAppl Var, ATerm Expr);
-		ATerm getSubstitution(ATermAppl Var);
+		void setSubstitution(ATermAppl Var, ATermAppl Expr);
+		void setSubstitutionList(ATermList Exprs);
+		void setSubstitutionInternal(ATermAppl Var, ATerm Expr);
+		void setSubstitutionInternalList(ATermList Exprs);
+		ATermAppl getSubstitution(ATermAppl Var);
+		ATerm getSubstitutionInternal(ATermAppl Var);
 		void clearSubstitution(ATermAppl Var);
 		void clearSubstitutions();
+		void clearSubstitutions(ATermList Vars);
 
 	public:
 		BDD_Prover *prover_obj;

libraries/data/include/mcrl2/data/rewrite.h
@@ -14,15 +14,30 @@
 #include <mcrl2/data/data_specification.h>
 #include <aterm2.h>
 
+#ifndef NO_DYNLOAD
+#define MCRL2_INNERC_AVAILABLE /** \brief If defined the compiling innermost
+                                          rewriter is available */
+#define MCRL2_JITTYC_AVAILABLE /** \brief If defined the compiling JITty
+                                          rewriter is available */
+#endif
+
 /** \brief Rewrite strategies. */
 typedef enum { GS_REWR_INNER     /** \brief Innermost */
+#ifdef MCRL2_INNERC_AVAILABLE
 	     , GS_REWR_INNERC    /** \brief Compiling innermost */
+#endif
 	     , GS_REWR_JITTY     /** \brief JITty */
+#ifdef MCRL2_JITTYC_AVAILABLE
 	     , GS_REWR_JITTYC    /** \brief Compiling JITty */
+#endif
 	     , GS_REWR_INNER_P   /** \brief Innermost + Prover */
+#ifdef MCRL2_INNERC_AVAILABLE
 	     , GS_REWR_INNERC_P  /** \brief Compiling innermost + Prover*/
+#endif
 	     , GS_REWR_JITTY_P   /** \brief JITty + Prover */
+#ifdef MCRL2_JITTYC_AVAILABLE
 	     , GS_REWR_JITTYC_P  /** \brief Compiling JITty + Prover*/
+#endif
 	     , GS_REWR_INVALID   /** \brief Invalid strategy */
 	     } RewriteStrategy;
 
@@ -50,16 +65,15 @@ typedef enum { GS_REWR_INNER     /** \brief Innermost */
  * Instead of first substituting specific values for variables before rewriting
  * a term, one can tell the rewriter to do this substitution during rewriting.
  * Typical usage would be as follows (with t an mCRL2 data term, var an mCRL2
- * data variable and values a list of terms in the internal rewriter format):
+ * data variable and values a list of mCRL2 data terms):
  *
  * \code
  *   Rewriter *r = createRewriter(equations);
- *   ATerm u = r->toRewriteFormat(t);
  *   for (iterator i = values.begin(); i != values.end(); i++)
  *   {
  *     r->setSubstitution(var,*i);
- *     ATerm v = t->rewriteInternal(u);
- *     // v is the normal form in internal rewriter format of t[var:=*i]
+ *     ATerm v = t->rewrite(t);
+ *     // v is the normal form in of t[var:=*i]
  *     ...
  *   }
  *   delete r;
@@ -152,17 +166,47 @@ class Rewriter
 		 * \brief Link a variable to a value for on-the-fly
 		 *        substitution. (Replacing any previous linked value.)
 		 * \param Var  A mCRL2 data variable.
+		 * \param Expr A mCRL2 data expression.
+		 **/
+		virtual void setSubstitution(ATermAppl Var, ATermAppl Expr);
+		/**
+		 * \brief Link variables to a values for on-the-fly
+		 *        substitution. (Replacing any previous linked value.)
+		 * \param Exprs A lists of substitutions of mCRL2 data variables
+                 *              to mCRL2 data expressions.
+		 **/
+		virtual void setSubstitutionList(ATermList Exprs);
+		/**
+		 * \brief Link a variable to a value for on-the-fly
+		 *        substitution. (Replacing any previous linked value.)
+		 * \param Var  A mCRL2 data variable.
 		 * \param Expr A term in the internal rewriter format.
 		 **/
-		virtual void setSubstitution(ATermAppl Var, ATerm Expr);
+		virtual void setSubstitutionInternal(ATermAppl Var, ATerm Expr);
+		/**
+		 * \brief Link variables to a values for on-the-fly
+		 *        substitution. (Replacing any previous linked value.)
+		 * \param Exprs A lists of substitutions of mCRL2 data variables
+                 *              to terms in the internal rewriter format.
+		 **/
+		virtual void setSubstitutionInternalList(ATermList Exprs);
 		/**
 		 * \brief Get the value linked to a variable for on-the-fly
 		 *        substitution.
 		 * \param Var A mCRL2 data variable.
-		 * \return The value linked to Var. If no value is linked to
-		 *         Var, then NULL is returned.
+		 * \return The value linked to Var as an mCRL2 data expression.
+                 *         If no value is linked to Var, then NULL is returned.
 		 **/
-		virtual ATerm getSubstitution(ATermAppl Var);
+		virtual ATermAppl getSubstitution(ATermAppl Var);
+		/**
+		 * \brief Get the value linked to a variable for on-the-fly
+		 *        substitution.
+		 * \param Var A mCRL2 data variable.
+		 * \return The value linked to Var as a term in the internal
+                 *         rewriter format. If no value is linked to Var,
+		 *         then NULL is returned.
+		 **/
+		virtual ATerm getSubstitutionInternal(ATermAppl Var);
 		/**
 		 * \brief Remove the value linked to a variable for on-the-fly
 		 *        substitution. (I.e. make sure that no value is
@@ -176,6 +220,13 @@ class Rewriter
 		 *        done during rewriting.)
 		 **/
 		virtual void clearSubstitutions();
+		/**
+		 * \brief Remove the values linked to variables for on-the-fly
+		 *        substitution. (I.e. make sure that no value is
+		 *        substituted for this variable during rewriting.)
+		 * \param Vars A list of mCRL2 data variable.
+		 **/
+		virtual void clearSubstitutions(ATermList Vars);
 
 	protected:
 		ATerm lookupSubstitution(ATermAppl Var);

libraries/data/source/rewrite/rewrite.cpp
@@ -20,8 +20,12 @@
 #include "mcrl2/data/rewrite.h"
 #include "mcrl2/data/detail/rewrite/inner.h"
 #include "mcrl2/data/detail/rewrite/jitty.h"
+#ifdef MCRL2_INNERC_AVAILABLE
 #include "mcrl2/data/detail/rewrite/innerc.h"
+#endif
+#ifdef MCRL2_JITTYC_AVAILABLE
 #include "mcrl2/data/detail/rewrite/jittyc.h"
+#endif
 #include "mcrl2/data/detail/rewrite/with_prover.h"
 
 using namespace mcrl2::utilities;
@@ -90,7 +94,21 @@ bool Rewriter::removeRewriteRule(ATermAppl /*Rule*/)
 	return false;
 }
 
-void Rewriter::setSubstitution(ATermAppl Var, ATerm Expr)
+void Rewriter::setSubstitution(ATermAppl Var, ATermAppl Expr)
+{
+	setSubstitutionInternal(Var,toRewriteFormat(Expr));
+}
+
+void Rewriter::setSubstitutionList(ATermList Exprs)
+{
+	for (; !ATisEmpty(Exprs); Exprs=ATgetNext(Exprs))
+	{
+		ATermAppl h = (ATermAppl) ATgetFirst(Exprs);
+		setSubstitutionInternal((ATermAppl) ATgetArgument(h,0),toRewriteFormat((ATermAppl) ATgetArgument(h,1)));
+	}
+}
+
+void Rewriter::setSubstitutionInternal(ATermAppl Var, ATerm Expr)
 {
 	long n = ATgetAFun(ATgetArgument(Var,0));
 	
@@ -134,7 +152,21 @@ void Rewriter::setSubstitution(ATermAppl Var, ATerm Expr)
 	substs[n] = Expr;
 }
 
-ATerm Rewriter::getSubstitution(ATermAppl Var)
+void Rewriter::setSubstitutionInternalList(ATermList Exprs)
+{
+	for (; !ATisEmpty(Exprs); Exprs=ATgetNext(Exprs))
+	{
+		ATermAppl h = (ATermAppl) ATgetFirst(Exprs);
+		setSubstitutionInternal((ATermAppl) ATgetArgument(h,0),ATgetArgument(h,1));
+	}
+}
+
+ATermAppl Rewriter::getSubstitution(ATermAppl Var)
+{
+	return fromRewriteFormat(lookupSubstitution(Var));
+}
+
+ATerm Rewriter::getSubstitutionInternal(ATermAppl Var)
 {
 	return lookupSubstitution(Var);
 }
@@ -154,6 +186,14 @@ void Rewriter::clearSubstitutions()
 	for (long i=0; i<substs_size; i++)
 	{
 		substs[i] = NULL;
+	}
+}
+
+void Rewriter::clearSubstitutions(ATermList Vars)
+{
+	for (; !ATisEmpty(Vars); Vars=ATgetNext(Vars))
+	{
+		clearSubstitution((ATermAppl) ATgetFirst(Vars));
 	}
 }
 
@@ -185,18 +225,26 @@ Rewriter *createRewriter(data_specification DataSpec, RewriteStrategy Strategy)
 			return new RewriterInnermost(DataSpec);
 		case GS_REWR_JITTY:
 			return new RewriterJitty(DataSpec);
+#ifdef MCRL2_INNERC_AVAILABLE
 		case GS_REWR_INNERC:
 			return new RewriterCompilingInnermost(DataSpec);
+#endif
+#ifdef MCRL2_JITTYC_AVAILABLE
 		case GS_REWR_JITTYC:
 			return new RewriterCompilingJitty(DataSpec);
+#endif
 		case GS_REWR_INNER_P:
 			return new RewriterProver(DataSpec,GS_REWR_INNER);
 		case GS_REWR_JITTY_P:
 			return new RewriterProver(DataSpec,GS_REWR_JITTY);
+#ifdef MCRL2_INNERC_AVAILABLE
 		case GS_REWR_INNERC_P:
 			return new RewriterProver(DataSpec,GS_REWR_INNERC);
+#endif
+#ifdef MCRL2_JITTYC_AVAILABLE
 		case GS_REWR_JITTYC_P:
 			return new RewriterProver(DataSpec,GS_REWR_JITTYC);
+#endif
 		default:
 			return NULL;
 	}
@@ -279,7 +327,7 @@ void CheckRewriteRule(ATermAppl DataEqn)
 		throw runtime_error("variable "+PrintPart_CXX((ATerm) var,ppDefault)+" occurs in left-hand side of equation but is not defined (in equation: "+PrintPart_CXX((ATerm) DataEqn,ppDefault)+")");
 	}
 
-	// check that variables from the rhs are occur in the lhs
+	// check that variables from the condition occur in the lhs
 	try
 	{
 		checkVars(ATAgetArgument(DataEqn,1),lhs_vars);
@@ -288,7 +336,7 @@ void CheckRewriteRule(ATermAppl DataEqn)
 		throw runtime_error("variable "+PrintPart_CXX((ATerm) var,ppDefault)+" occurs in condition of equation but not in left-hand side (in equation: "+PrintPart_CXX((ATerm) DataEqn,ppDefault)+"); equation cannot be used as rewrite rule");
 	}
 
-	// check that variables from the condition occur in the lhs
+	// check that variables from the rhs are occur in the lhs
 	try
 	{
 		checkVars(ATAgetArgument(DataEqn,3),lhs_vars);
@@ -298,6 +346,10 @@ void CheckRewriteRule(ATermAppl DataEqn)
 	}
 
 	// check that the lhs is a supported pattern
+	if ( gsIsDataVarId(ATAgetArgument(DataEqn,2)) )
+	{
+		throw runtime_error("left-hand side of equation is a variable; this is not allowed for rewriting");
+	}
 	try
 	{
 		checkPattern(ATAgetArgument(DataEqn,2));
@@ -323,20 +375,28 @@ void PrintRewriteStrategy(FILE *stream, RewriteStrategy strat)
 {
   if (strat == GS_REWR_INNER) {
     fprintf(stream, "inner");
+#ifdef MCRL2_INNERC_AVAILABLE
   } else if (strat == GS_REWR_INNERC) {
     fprintf(stream, "innerc");
+#endif
   } else if (strat == GS_REWR_JITTY) {
     fprintf(stream, "jitty");
+#ifdef MCRL2_JITTYC_AVAILABLE
   } else if (strat == GS_REWR_JITTYC) {
     fprintf(stream, "jittyc");
+#endif
   } else if (strat == GS_REWR_INNER_P) {
     fprintf(stream, "innerp");
+#ifdef MCRL2_INNERC_AVAILABLE
   } else if (strat == GS_REWR_INNERC_P) {
     fprintf(stream, "innercp");
+#endif
   } else if (strat == GS_REWR_JITTY_P) {
     fprintf(stream, "jittyp");
+#ifdef MCRL2_JITTYC_AVAILABLE
   } else if (strat == GS_REWR_JITTYC_P) {
     fprintf(stream, "jittycp");
+#endif
   } else {
     fprintf(stream, "invalid");
   }
@@ -345,8 +405,16 @@ void PrintRewriteStrategy(FILE *stream, RewriteStrategy strat)
 RewriteStrategy RewriteStrategyFromString(const char *s)
 {
   static RewriteStrategy strategies[9] = { GS_REWR_INVALID,
+#ifdef MCRL2_INNERC_AVAILABLE
           GS_REWR_INNER, GS_REWR_INNERC, GS_REWR_INNER_P, GS_REWR_INNERC_P,
+#else
+          GS_REWR_INNER, GS_REWR_INVALID, GS_REWR_INNER_P, GS_REWR_INVALID,
+#endif
+#ifdef MCRL2_JITTYC_AVAILABLE
           GS_REWR_JITTY, GS_REWR_JITTYC, GS_REWR_JITTY_P, GS_REWR_JITTYC_P };
+#else
+          GS_REWR_JITTY, GS_REWR_INVALID, GS_REWR_JITTY_P, GS_REWR_INVALID };
+#endif
 
   size_t main_strategy = 0; // default invalid
 

libraries/data/source/rewrite/with_prover.cpp
@@ -78,14 +78,34 @@ ATermAppl RewriterProver::fromRewriteFormat(ATerm Term)
   return rewr_obj->fromRewriteFormat(Term);
 }
 
-void RewriterProver::setSubstitution(ATermAppl Var, ATerm Expr)
+void RewriterProver::setSubstitution(ATermAppl Var, ATermAppl Expr)
 {
   return rewr_obj->setSubstitution(Var,Expr);
 }
 
-ATerm RewriterProver::getSubstitution(ATermAppl Var)
+void RewriterProver::setSubstitutionList(ATermList Exprs)
+{
+  return rewr_obj->setSubstitutionList(Exprs);
+}
+
+void RewriterProver::setSubstitutionInternal(ATermAppl Var, ATerm Expr)
+{
+  return rewr_obj->setSubstitutionInternal(Var,Expr);
+}
+
+void RewriterProver::setSubstitutionInternalList(ATermList Exprs)
+{
+  return rewr_obj->setSubstitutionInternalList(Exprs);
+}
+
+ATermAppl RewriterProver::getSubstitution(ATermAppl Var)
 {
   return rewr_obj->getSubstitution(Var);
+}
+
+ATerm RewriterProver::getSubstitutionInternal(ATermAppl Var)
+{
+  return rewr_obj->getSubstitutionInternal(Var);
 }
 
 void RewriterProver::clearSubstitution(ATermAppl Var)
@@ -98,6 +118,11 @@ void RewriterProver::clearSubstitutions()
   return rewr_obj->clearSubstitutions();
 }
 
+void RewriterProver::clearSubstitutions(ATermList Vars)
+{
+  return rewr_obj->clearSubstitutions(Vars);
+}
+
 RewriteStrategy RewriterProver::getStrategy()
 {
   switch ( rewr_obj->getStrategy() )
@@ -106,10 +131,14 @@ RewriteStrategy RewriterProver::getStrategy()
       return GS_REWR_INNER_P;
     case GS_REWR_JITTY:
       return GS_REWR_JITTY_P;
+#ifdef MCRL2_INNERC_AVAILABLE
     case GS_REWR_INNERC:
       return GS_REWR_INNERC_P;
+#endif
+#ifdef MCRL2_JITTYC_AVAILABLE
     case GS_REWR_JITTYC:
       return GS_REWR_JITTYC_P;
+#endif
     default:
       return GS_REWR_INVALID;
   }

libraries/lps/source/nextstate_standard.cpp
@@ -755,11 +755,11 @@ ATerm NextStateGeneratorStandard::makeNewState(ATerm old, ATermList assigns)
 					break;
 				case GS_STATE_TREE:
 //					stateargs[i] = getTreeElement(old,i);
-					stateargs[i] = info.rewr_obj->getSubstitution((ATermAppl) ATgetFirst(l));
+					stateargs[i] = info.rewr_obj->getSubstitutionInternal((ATermAppl) ATgetFirst(l));
 					if ( ATisEqual(stateargs[i], ATgetFirst(l)) ) // Make sure substitutions where not reset by enumerator
 					{
 						set_substitutions();
-						stateargs[i] = info.rewr_obj->getSubstitution((ATermAppl) ATgetFirst(l));
+						stateargs[i] = info.rewr_obj->getSubstitutionInternal((ATermAppl) ATgetFirst(l));
 					}
 					break;
 			}
@@ -811,7 +811,7 @@ void NextStateGeneratorStandard::SetTreeStateVars(ATerm tree, ATermList *vars)
 		}
 	}
 
-	info.rewr_obj->setSubstitution((ATermAppl) ATgetFirst(*vars),tree);
+	info.rewr_obj->setSubstitutionInternal((ATermAppl) ATgetFirst(*vars),tree);
 	*vars = ATgetNext(*vars);
 }
 
@@ -865,7 +865,7 @@ void NextStateGeneratorStandard::set_substitutions()
 
 				if ( !ATisEqual(a,info.nil) )
 				{
-					info.rewr_obj->setSubstitution((ATermAppl) ATgetFirst(l),a);
+					info.rewr_obj->setSubstitutionInternal((ATermAppl) ATgetFirst(l),a);
 				}
 			}
 			break;
@@ -930,20 +930,14 @@ bool NextStateGeneratorStandard::next(ATermAppl *Transition, ATerm *State, bool 
 		{
 			set_substitutions();
 		}
-		for (ATermList m=sol; !ATisEmpty(m); m=ATgetNext(m))
-		{
-			info.rewr_obj->setSubstitution((ATermAppl) ATgetArgument((ATermAppl) ATgetFirst(m),0),ATgetArgument((ATermAppl) ATgetFirst(m),1));
-		}
+		info.rewr_obj->setSubstitutionInternalList(sol);
 		*Transition = rewrActionArgs((ATermAppl) cur_act);
 		*State = (ATerm) makeNewState(cur_state,cur_nextstate);
 		if ( prioritised != NULL )
 		{
 			*prioritised = (sum_idx <= info.num_prioritised);
 		}
-		for (ATermList m=sol; !ATisEmpty(m); m=ATgetNext(m))
-		{
-			info.rewr_obj->clearSubstitution((ATermAppl) ATgetArgument((ATermAppl) ATgetFirst(m),0));
-		}
+		info.rewr_obj->clearSubstitutions(sol);
 		return true;
 	} else {
 		*Transition = NULL;

libraries/data/include/mcrl2/data/detail/rewrite/innerc.h
@@ -12,6 +12,9 @@
 #define __REWR_INNERC_H
 
 #include "mcrl2/data/rewrite.h"
+
+#ifdef MCRL2_INNERC_AVAILABLE
+
 #include "mcrl2/data/data_specification.h"
 
 //#define _INNERC_STORE_TREES
@@ -31,12 +34,11 @@ class RewriterCompilingInnermost: public Rewriter
 		ATerm rewriteInternal(ATerm Term);
 		ATermList rewriteInternalList(ATermList Terms);
 
-		void setSubstitution(ATermAppl Var, ATerm Expr);
-		ATerm getSubstitution(ATermAppl Var);
+		void setSubstitutionInternal(ATermAppl Var, ATerm Expr);
+		ATerm getSubstitutionInternal(ATermAppl Var);
 		void clearSubstitution(ATermAppl Var);
 		void clearSubstitutions();
 
-#ifndef NO_DYNLOAD
 	private:
 		int num_opids;
 
@@ -71,7 +73,8 @@ class RewriterCompilingInnermost: public Rewriter
 		ATerm OpId2Int(ATermAppl Term, bool add_opids);
 		ATerm toInner(ATermAppl Term, bool add_opids);
 		ATermAppl fromInner(ATerm Term);
-#endif
 };
+
+#endif
 
 #endif

libraries/data/include/mcrl2/data/detail/rewrite/jittyc.h
@@ -11,10 +11,13 @@
 #ifndef __REWR_JITTYC_H
 #define __REWR_JITTYC_H
 
+#include "mcrl2/data/rewrite.h"
+
+#ifdef MCRL2_JITTYC_AVAILABLE
+
 //#define _JITTYC_STORE_TREES
 #include <utility>
 #include <string>
-#include "mcrl2/data/rewrite.h"
 
 typedef unsigned int* nfs_array;
 
@@ -33,12 +36,11 @@ class RewriterCompilingJitty: public Rewriter
 		ATerm rewriteInternal(ATerm Term);
 		ATermList rewriteInternalList(ATermList Terms);
 
-		void setSubstitution(ATermAppl Var, ATerm Expr);
-		ATerm getSubstitution(ATermAppl Var);
+		void setSubstitutionInternal(ATermAppl Var, ATerm Expr);
+		ATerm getSubstitutionInternal(ATermAppl Var);
 		void clearSubstitution(ATermAppl Var);
 		void clearSubstitutions();
 
-#ifndef NO_DYNLOAD
 	private:
 		int num_opids;
 
@@ -92,7 +94,8 @@ class RewriterCompilingJitty: public Rewriter
 		ATerm OpId2Int(ATermAppl Term, bool add_opids);
 		ATerm toInner(ATermAppl Term, bool add_opids);
 		ATermAppl fromInner(ATerm Term);
-#endif
 };
+
+#endif
 
 #endif

libraries/data/include/mcrl2/data/rewriter.h
@@ -49,13 +49,23 @@ namespace data {
       enum strategy
       {
         innermost                  = GS_REWR_INNER   ,  /** \brief Innermost */
+#ifdef MCRL2_INNERC_AVAILABLE
   	    innermost_compiling        = GS_REWR_INNERC  ,  /** \brief Compiling innermost */
+#endif
   	    jitty                      = GS_REWR_JITTY   ,  /** \brief JITty */
+#ifdef MCRL2_JITTYC_AVAILABLE
   	    jitty_compiling            = GS_REWR_JITTYC  ,  /** \brief Compiling JITty */
+#endif
   	    innermost_prover           = GS_REWR_INNER_P ,  /** \brief Innermost + Prover */
+#ifdef MCRL2_INNERC_AVAILABLE
   	    innermost_compiling_prover = GS_REWR_INNERC_P,  /** \brief Compiling innermost + Prover*/
+#endif
+#ifdef MCRL2_JITTYC_AVAILABLE
   	    jitty_prover               = GS_REWR_JITTY_P ,  /** \brief JITty + Prover */
   	    jitty_compiling_prover     = GS_REWR_JITTYC_P   /** \brief Compiling JITty + Prover*/
+#else
+  	    jitty_prover               = GS_REWR_JITTY_P    /** \brief JITty + Prover */
+#endif
       };
 
       /// \brief Constructor.

commit 3eef81482a3fa33ad00558130bb5ed415ed27b99

- Fix bug introduced in r5272; use of clearSubstitutions() in NextState was   incorrect. - Corrected naming of the parameter of setSubstitutionList() and   setSubstitutionInternalList().
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5274 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/data/include/mcrl2/data/detail/rewrite/with_prover.h
@@ -35,9 +35,9 @@ class RewriterProver: public Rewriter
 		bool removeRewriteRule(ATermAppl Rule);
 		
 		void setSubstitution(ATermAppl Var, ATermAppl Expr);
-		void setSubstitutionList(ATermList Exprs);
+		void setSubstitutionList(ATermList Substs);
 		void setSubstitutionInternal(ATermAppl Var, ATerm Expr);
-		void setSubstitutionInternalList(ATermList Exprs);
+		void setSubstitutionInternalList(ATermList Substs);
 		ATermAppl getSubstitution(ATermAppl Var);
 		ATerm getSubstitutionInternal(ATermAppl Var);
 		void clearSubstitution(ATermAppl Var);

libraries/data/include/mcrl2/data/rewrite.h
@@ -172,10 +172,10 @@ class Rewriter
 		/**
 		 * \brief Link variables to a values for on-the-fly
 		 *        substitution. (Replacing any previous linked value.)
-		 * \param Exprs A lists of substitutions of mCRL2 data variables
-                 *              to mCRL2 data expressions.
+		 * \param Substs A lists of substitutions of mCRL2 data
+                 *               variables to mCRL2 data expressions.
 		 **/
-		virtual void setSubstitutionList(ATermList Exprs);
+		virtual void setSubstitutionList(ATermList Substs);
 		/**
 		 * \brief Link a variable to a value for on-the-fly
 		 *        substitution. (Replacing any previous linked value.)
@@ -186,10 +186,11 @@ class Rewriter
 		/**
 		 * \brief Link variables to a values for on-the-fly
 		 *        substitution. (Replacing any previous linked value.)
-		 * \param Exprs A lists of substitutions of mCRL2 data variables
-                 *              to terms in the internal rewriter format.
+		 * \param Substs A lists of substitutions of mCRL2 data
+                 *               variables to terms in the internal rewriter
+                 *               format.
 		 **/
-		virtual void setSubstitutionInternalList(ATermList Exprs);
+		virtual void setSubstitutionInternalList(ATermList Substs);
 		/**
 		 * \brief Get the value linked to a variable for on-the-fly
 		 *        substitution.

libraries/data/source/rewrite/rewrite.cpp
@@ -99,11 +99,11 @@ void Rewriter::setSubstitution(ATermAppl Var, ATermAppl Expr)
 	setSubstitutionInternal(Var,toRewriteFormat(Expr));
 }
 
-void Rewriter::setSubstitutionList(ATermList Exprs)
+void Rewriter::setSubstitutionList(ATermList Substs)
 {
-	for (; !ATisEmpty(Exprs); Exprs=ATgetNext(Exprs))
+	for (; !ATisEmpty(Substs); Substs=ATgetNext(Substs))
 	{
-		ATermAppl h = (ATermAppl) ATgetFirst(Exprs);
+		ATermAppl h = (ATermAppl) ATgetFirst(Substs);
 		setSubstitutionInternal((ATermAppl) ATgetArgument(h,0),toRewriteFormat((ATermAppl) ATgetArgument(h,1)));
 	}
 }
@@ -152,11 +152,11 @@ void Rewriter::setSubstitutionInternal(ATermAppl Var, ATerm Expr)
 	substs[n] = Expr;
 }
 
-void Rewriter::setSubstitutionInternalList(ATermList Exprs)
+void Rewriter::setSubstitutionInternalList(ATermList Substs)
 {
-	for (; !ATisEmpty(Exprs); Exprs=ATgetNext(Exprs))
+	for (; !ATisEmpty(Substs); Substs=ATgetNext(Substs))
 	{
-		ATermAppl h = (ATermAppl) ATgetFirst(Exprs);
+		ATermAppl h = (ATermAppl) ATgetFirst(Substs);
 		setSubstitutionInternal((ATermAppl) ATgetArgument(h,0),ATgetArgument(h,1));
 	}
 }

libraries/data/source/rewrite/with_prover.cpp
@@ -83,9 +83,9 @@ void RewriterProver::setSubstitution(ATermAppl Var, ATermAppl Expr)
   return rewr_obj->setSubstitution(Var,Expr);
 }
 
-void RewriterProver::setSubstitutionList(ATermList Exprs)
+void RewriterProver::setSubstitutionList(ATermList Substs)
 {
-  return rewr_obj->setSubstitutionList(Exprs);
+  return rewr_obj->setSubstitutionList(Substs);
 }
 
 void RewriterProver::setSubstitutionInternal(ATermAppl Var, ATerm Expr)
@@ -93,9 +93,9 @@ void RewriterProver::setSubstitutionInternal(ATermAppl Var, ATerm Expr)
   return rewr_obj->setSubstitutionInternal(Var,Expr);
 }
 
-void RewriterProver::setSubstitutionInternalList(ATermList Exprs)
+void RewriterProver::setSubstitutionInternalList(ATermList Substs)
 {
-  return rewr_obj->setSubstitutionInternalList(Exprs);
+  return rewr_obj->setSubstitutionInternalList(Substs);
 }
 
 ATermAppl RewriterProver::getSubstitution(ATermAppl Var)

libraries/lps/source/nextstate_standard.cpp
@@ -937,7 +937,10 @@ bool NextStateGeneratorStandard::next(ATermAppl *Transition, ATerm *State, bool 
 		{
 			*prioritised = (sum_idx <= info.num_prioritised);
 		}
-		info.rewr_obj->clearSubstitutions(sol);
+		for (ATermList m=sol; !ATisEmpty(m); m=ATgetNext(m))
+		{
+			info.rewr_obj->clearSubstitution((ATermAppl) ATgetArgument((ATermAppl) ATgetFirst(m),0));
+		} 
 		return true;
 	} else {
 		*Transition = NULL;