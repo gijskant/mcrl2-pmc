gijskant/mcrl2-pmc

commit 753280f494c2d6278197f0193b2d65fc9f42dfaf

Changes:  - fixes and workarounds for compiler warnings (gcc release mode)  - moved wxWidgets utility functionality to mcrl2::utilities::wx namespace  - rewriter.h: adjusted white-space (removed tabs)
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5448 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/core/source/struct.cpp
@@ -41,11 +41,13 @@ static bool IsPNSort(ATermAppl SortExpr);
 static bool IsPNISort(ATermAppl SortExpr);
 //Ret: SortExpr is a sort expression for Pos, Nat or Int
 
+#ifndef NDEBUG
 static bool IsNISort(ATermAppl SortExpr);
 //Ret: SortExpr is a sort expression for Nat or Int
 
 static bool IsPNIRSort(ATermAppl SortExpr);
 //Ret: SortExpr is a sort expression for Pos, Nat, Int or Real
+#endif
 
 static ATermAppl IntersectPNSorts(ATermAppl SortExpr1, ATermAppl SortExpr2);
 //Pre: IsPNSort(SortExpr1) and IsPNSort(SortExpr2)
@@ -3943,7 +3945,6 @@ bool gsIsFixpoint(ATermAppl Term)
 
 //Local declarations
 //------------------
-
 bool IsPNSort(ATermAppl SortExpr)
 {
   return
@@ -3958,6 +3959,7 @@ bool IsPNISort(ATermAppl SortExpr)
     ATisEqual(SortExpr, gsMakeSortExprInt());
 }
 
+#ifndef NDEBUG
 bool IsNISort(ATermAppl SortExpr)
 {
   return
@@ -3971,6 +3973,7 @@ bool IsPNIRSort(ATermAppl SortExpr)
     IsPNISort(SortExpr) ||
     ATisEqual(SortExpr, gsMakeSortExprReal());
 }
+#endif
 
 ATermAppl IntersectPNSorts(ATermAppl SortExpr1, ATermAppl SortExpr2)
 {

libraries/data/include/mcrl2/data/rewriter.h
@@ -126,26 +126,26 @@ namespace data {
         : basic_rewriter<data_expression>(d, s)
       { }
 
-		  /// \brief Rewrites a data expression.
-		  /// \param d The term to be rewritten.
-		  /// \return The normal form of d.
-		  ///
-		  data_expression operator()(const data_expression& d) const
-		  {
-		    return m_rewriter.get()->rewrite(d);
-		  }
+      /// \brief Rewrites a data expression.
+      /// \param d The term to be rewritten.
+      /// \return The normal form of d.
+      ///
+      data_expression operator()(const data_expression& d) const
+      {
+        return m_rewriter.get()->rewrite(d);
+      }
 
-		  /// \brief Rewrites the data expression d, and on the fly applies a substitution function
-		  /// to data variables.
-		  /// \param d A term.
-		  /// \param sigma A substitution function.
-		  /// \return The normal form of the term.
-		  ///
-		  template <typename SubstitutionFunction>
-		  data_expression operator()(const data_expression& d, SubstitutionFunction sigma) const
-		  {
-		    return this->operator()(replace_data_variables(d, sigma));
-		  }
+      /// \brief Rewrites the data expression d, and on the fly applies a substitution function
+      /// to data variables.
+      /// \param d A term.
+      /// \param sigma A substitution function.
+      /// \return The normal form of the term.
+      ///
+      template <typename SubstitutionFunction>
+      data_expression operator()(const data_expression& d, SubstitutionFunction sigma) const
+      {
+        return this->operator()(replace_data_variables(d, sigma));
+      }
   };
 
   /// \brief Rewriter that operates on data expressions.
@@ -165,31 +165,31 @@ namespace data {
         : basic_rewriter<data_expression_with_variables>(r.m_rewriter)
       {}
 
-		  /// \brief Rewrites a data expression.
-		  /// \param d The term to be rewritten.
-		  /// \return The normal form of d.
-		  ///
-		  data_expression_with_variables operator()(const data_expression_with_variables& d) const
-		  {
-		    data_expression t = m_rewriter.get()->rewrite(d);
-		    std::set<data_variable> v = find_all_data_variables(t);
-		    return data_expression_with_variables(t, data_variable_list(v.begin(), v.end()));
-		  }
+      /// \brief Rewrites a data expression.
+      /// \param d The term to be rewritten.
+      /// \return The normal form of d.
+      ///
+      data_expression_with_variables operator()(const data_expression_with_variables& d) const
+      {
+        data_expression t = m_rewriter.get()->rewrite(d);
+        std::set<data_variable> v = find_all_data_variables(t);
+        return data_expression_with_variables(t, data_variable_list(v.begin(), v.end()));
+      }
 
-		  /// \brief Rewrites the data expression d, and on the fly applies a substitution function
-		  /// to data variables.
-		  /// \param d A term.
-		  /// \param sigma A substitution function.
-		  /// \return The normal form of the term.
-		  ///
-		  template <typename SubstitutionFunction>
-		  data_expression_with_variables operator()(const data_expression_with_variables& d, SubstitutionFunction sigma) const
-		  {
-		    data_expression t = this->operator()(replace_data_variables(d, sigma));
-		    std::set<data_variable> v = find_all_data_variables(t);
-		    data_expression_with_variables result(t, data_variable_list(v.begin(), v.end()));
-		    return result;
-		  }
+      /// \brief Rewrites the data expression d, and on the fly applies a substitution function
+      /// to data variables.
+      /// \param d A term.
+      /// \param sigma A substitution function.
+      /// \return The normal form of the term.
+      ///
+      template <typename SubstitutionFunction>
+      data_expression_with_variables operator()(const data_expression_with_variables& d, SubstitutionFunction sigma) const
+      {
+        data_expression t = this->operator()(replace_data_variables(d, sigma));
+        std::set<data_variable> v = find_all_data_variables(t);
+        data_expression_with_variables result(t, data_variable_list(v.begin(), v.end()));
+        return result;
+      }
   };
 
   /// \brief Function object that turns a map of substitutions to variables into a substitution function.

libraries/utilities/include/mcrl2/utilities/colorbutton.h
@@ -16,35 +16,37 @@
 
 namespace mcrl2 {
   namespace utilities {
+    namespace wx {
 
-    /**
-     * \brief A widget for choosing colours
-     *
-     * The widget consists of a small square with the selected colour as
-     * background.  Clicking the square will activate a colour picking dialog.
-     * The colour that is selected in the dialog is taken as the new colour of
-     * the square.
-     **/
-    class wxColorButton : public wxPanel {
+      /**
+       * \brief A widget for choosing colours
+       *
+       * The widget consists of a small square with the selected colour as
+       * background.  Clicking the square will activate a colour picking dialog.
+       * The colour that is selected in the dialog is taken as the new colour of
+       * the square.
+       **/
+      class wxColorButton : public wxPanel {
 
-      protected:
-    
-        /// \brief Window used to centering a colour dialog
-        wxWindow* appFrame;
-    
-        DECLARE_EVENT_TABLE()
+        protected:
 
-      private:
-    
-        /// \brief Event handler for mouse down events
-        void OnMouseDown( wxMouseEvent& event );
-    
-      public:
-    
-        /// \brief Constructor
-        wxColorButton( wxWindow* parent, wxWindow* topframe, wxWindowID id = wxID_ANY,
-                        const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize );
-    };
+          /// \brief Window used to centering a colour dialog
+          wxWindow* appFrame;
+
+          DECLARE_EVENT_TABLE()
+
+        private:
+
+          /// \brief Event handler for mouse down events
+          void OnMouseDown( wxMouseEvent& event );
+
+        public:
+
+          /// \brief Constructor
+          wxColorButton( wxWindow* parent, wxWindow* topframe, wxWindowID id = wxID_ANY,
+                          const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize );
+      };
+    }
   }
 }
 

libraries/utilities/include/mcrl2/utilities/command_line_interface.h
@@ -20,6 +20,7 @@
 #include <sstream>
 
 #include "boost/algorithm/string.hpp"
+#include "boost/type_traits/is_pod.hpp"
 #include "boost/shared_ptr.hpp"
 
 namespace mcrl2 {
@@ -45,6 +46,32 @@ namespace mcrl2 {
 
       void register_rewriting_options(interface_description&);
       void register_proving_options(interface_description&);
+
+      /// Helper class to prevent uninitialised variable warnings
+      template < typename T, bool = boost::is_pod< T >::value >
+      struct instance_of;
+
+      template < typename T >
+      struct instance_of< T, true > {
+        T m_wrapped;
+
+        instance_of() {
+          m_wrapped = static_cast< T >(0);
+        }
+
+        T& wrapped() {
+          return m_wrapped;
+        }
+      };
+
+      template < typename T >
+      struct instance_of< T, false > {
+        T m_wrapped;
+
+        T& wrapped() {
+          return m_wrapped;
+        }
+      };
     }
     // \endcond
 
@@ -802,9 +829,9 @@ namespace mcrl2 {
         inline T option_argument_as(std::string const& long_identifier) const {
           std::istringstream in(option_argument(long_identifier));
 
-          T result;
+          detail::instance_of< T > result;
 
-          in >> result;
+          in >> result.wrapped();
 
           if (in.fail()) {
             const char short_option(m_interface.long_to_short(long_identifier));
@@ -813,7 +840,7 @@ namespace mcrl2 {
                 ((short_option == '\0') ? " " : " or -" + std::string(1, short_option)) + " is invalid");
           }
 
-          return result;
+          return result.wrapped();
         }
     };
 

libraries/utilities/include/mcrl2/utilities/font_renderer.h
@@ -13,173 +13,177 @@
 #define FONT_RENDERER_H
 
 #include <string>
+#include <wx/wx.h>
 #include <wx/glcanvas.h>
 
 namespace mcrl2
 {
   namespace utilities
   {
-    /**
-     * \brief Type of text alignment. This contains both vertical and horizontal
-     *        alignments.
-     **/
-    enum Alignment
-    {          al_top,
-      al_left, al_center, al_right,
-               al_bottom
-    };
-
-    /**
-     * \brief Number of characters within the character set.
-     **/
-    const size_t CHARSETSIZE = 96;
-
-    /**
-     * \brief Pixel width per character.
-     **/
-    const size_t CHARWIDTH = 16;
-
-    /**
-     * \brief Pixel height per character.
-     **/
-    const size_t CHARHEIGHT = 32;
-
-    /**
-     * \brief A font renderer for use with OpenGL
-     *
-     * The font renderer maintains the resources used for drawing text on an 
-     * OpenGL canvas. This is intended for wxGL implementations, but will 
-     * probably work on generic OpenGL versions.
-     * The renderer provides three functions: 
-     *   - draw a string,
-     *   - draw a string cropped to a specified bounding box,
-     *   - draw the bounding box of a string
-     **/
-    class font_renderer
+    namespace wx
     {
-      public:
-        /// \brief Constructor
-        font_renderer();
+      /**
+       * \brief Type of text alignment. This contains both vertical and horizontal
+       *        alignments.
+       **/
+      enum Alignment
+      {          al_top,
+        al_left, al_center, al_right,
+                 al_bottom
+      };
 
-        /// \brief Destructor
-        ~font_renderer();
+      /**
+       * \brief Number of characters within the character set.
+       **/
+      const size_t CHARSETSIZE = 96;
 
-        /// \brief Renders the string s.
-        /// \param[in] s The string to be drawn.
-        /// \param[in] x The x coordinate of the string.
-        /// \param[in] y The y coordinate of the string.
-        /// \param[in] scale The scale factor for the text.
-        /// \param[in] align_horizontal The horizontal alignment of the text.
-        ///            If the alignment is not one of {left, center, right}, the
-        ///            text defaults to right-aligned.
-        /// \param[in] align_vertical The vertical alignment of the text.
-        ///            If the alignment is not one of {top, center, bottom}, the
-        ///            text defaults to center-aligned.
-        /// \pre There is an initialized canvas, text color is initialized and
-        ///      any necessary rotation has been done.
-        ///
-        /// \post The text is rendered at the specified location, with the 
-        ///       specified alignments and at the specified scale.
-        void draw_text(
-          const std::string s,
-          const double x, 
-          const double y,
-          const double scale,
-          const Alignment align_horizontal,
-          const Alignment align_vertical
-        );
+      /**
+       * \brief Pixel width per character.
+       **/
+      const size_t CHARWIDTH = 16;
 
-        /// \brief Renders a bounding box for string s.
-        /// \param[in] s The string to be drawn.
-        /// \param[in] x The x coordinate of the string.
-        /// \param[in] y The y coordinate of the string.
-        /// \param[in] scale The scale factor for the text.
-        /// \param[in] align_horizontal The horizontal alignment of the text.
-        /// \param[in] align_vertical The vertical alignment of the text.
-        /// \param[in] draw_border Whether or not to draw a (black) border.
-        ///
-        /// \pre There is an initialized canvas, box color is initialized and
-        ///      any necessary rotation has been done.
-        ///
-        /// \post A bounding box for the given string is drawn.
-        void draw_bounding_box(
-          const std::string s,
-          const double x, 
-          const double y,
-          const double scale,
-          const Alignment align_horizontal,
-          const Alignment align_vertical,
-          const bool draw_border
-        );
+      /**
+       * \brief Pixel height per character.
+       **/
+      const size_t CHARHEIGHT = 32;
 
-        /// \brief Renders the string s.
-        /// \param[in] s The string to be drawn.
-        /// \param[in] x The x coordinate of the string.
-        /// \param[in] y The y coordinate of the string.
-        /// \param[in] xLft The left side of the bounding box.
-        /// \param[in] xRgt The right side of the bounding box.
-        /// \param[in] yBot The bottom of the bounding box.
-        /// \param[in] yTop The top of the bounding box.
-        /// \param[in] scale The scale factor for the text.
-        /// \param[in] align_horizontal The horizontal alignment of the text.
-        /// \param[in] align_vertical The vertical alignment of the text.
-        ///
-        /// \pre There is an initialized canvas, text color is initialized and
-        ///      any necessary rotation has been done.
-        ///
-        /// \post The text is rendered at the specified location, with the 
-        ///       specified alignments and at the specified scale. It is cropped
-        ///       To fit within the bounding box defined by xLft, xRgt, yBot and
-        ///       yTop.
-        void draw_cropped_text(
-          const std::string s,
-          const double x,
-          const double y,
-          const double xLft,
-          const double xRgt,
-          const double yTop,
-          const double yBot,
-          const double scale,
-          const Alignment align_horizontal,
-          const Alignment align_vertical
-        );
-       
-        /// \brief Renders the string s wrapped into a bounding box.
-        /// \param[in] s The string to be drawn.
-        /// \param[in] xLft The left side of the bounding box.
-        /// \param[in] xRgt The right side of the bounding box.
-        /// \param[in] yBot The bottom of the bounding box.
-        /// \param[in] yTop The top of the bounding box.
-        /// \param[in] scale The scale factor for the text.
-        /// \param[in] align_horizontal The horizontal alignment of the text.
-        /// \param[in] align_vertical The vertical alignment of the text.
-        ///
-        /// \pre There is an initialized canvas, text color is initialized and
-        ///      any necessary rotation has been done.
-        ///
-        /// \post The text is rendered at the specified location, with the 
-        ///       specified alignments and at the specified scale. It is cropped
-        ///       To fit within the bounding box defined by xLft, xRgt, yBot and
-        ///       yTop.
-        void draw_wrapped_text(
-          const std::string s,
-          const double xLft, 
-          const double xRgt,
-          const double yTop, 
-          const double yBot,
-          const double scale,
-          const Alignment align_horizontal,
-          const Alignment align_vertical
-        );
+      /**
+       * \brief A font renderer for use with OpenGL
+       *
+       * The font renderer maintains the resources used for drawing text on an 
+       * OpenGL canvas. This is intended for wxGL implementations, but will 
+       * probably work on generic OpenGL versions.
+       * The renderer provides three functions: 
+       *   - draw a string,
+       *   - draw a string cropped to a specified bounding box,
+       *   - draw the bounding box of a string
+       **/
+      class font_renderer
+      {
+        public:
+          /// \brief Constructor
+          font_renderer();
 
-        private:
-          static GLuint  (&character_texture_id())[CHARSETSIZE];
-          static GLubyte (&character_textures())[CHARSETSIZE][CHARHEIGHT * CHARWIDTH];
+          /// \brief Destructor
+          ~font_renderer();
 
-          size_t index_from_char(const char & c);
+          /// \brief Renders the string s.
+          /// \param[in] s The string to be drawn.
+          /// \param[in] x The x coordinate of the string.
+          /// \param[in] y The y coordinate of the string.
+          /// \param[in] scale The scale factor for the text.
+          /// \param[in] align_horizontal The horizontal alignment of the text.
+          ///            If the alignment is not one of {left, center, right}, the
+          ///            text defaults to right-aligned.
+          /// \param[in] align_vertical The vertical alignment of the text.
+          ///            If the alignment is not one of {top, center, bottom}, the
+          ///            text defaults to center-aligned.
+          /// \pre There is an initialized canvas, text color is initialized and
+          ///      any necessary rotation has been done.
+          ///
+          /// \post The text is rendered at the specified location, with the 
+          ///       specified alignments and at the specified scale.
+          void draw_text(
+            const std::string s,
+            const double x, 
+            const double y,
+            const double scale,
+            const Alignment align_horizontal,
+            const Alignment align_vertical
+          );
 
-          bool initialise();
-    }; // class font_renderer
+          /// \brief Renders a bounding box for string s.
+          /// \param[in] s The string to be drawn.
+          /// \param[in] x The x coordinate of the string.
+          /// \param[in] y The y coordinate of the string.
+          /// \param[in] scale The scale factor for the text.
+          /// \param[in] align_horizontal The horizontal alignment of the text.
+          /// \param[in] align_vertical The vertical alignment of the text.
+          /// \param[in] draw_border Whether or not to draw a (black) border.
+          ///
+          /// \pre There is an initialized canvas, box color is initialized and
+          ///      any necessary rotation has been done.
+          ///
+          /// \post A bounding box for the given string is drawn.
+          void draw_bounding_box(
+            const std::string s,
+            const double x, 
+            const double y,
+            const double scale,
+            const Alignment align_horizontal,
+            const Alignment align_vertical,
+            const bool draw_border
+          );
+
+          /// \brief Renders the string s.
+          /// \param[in] s The string to be drawn.
+          /// \param[in] x The x coordinate of the string.
+          /// \param[in] y The y coordinate of the string.
+          /// \param[in] xLft The left side of the bounding box.
+          /// \param[in] xRgt The right side of the bounding box.
+          /// \param[in] yBot The bottom of the bounding box.
+          /// \param[in] yTop The top of the bounding box.
+          /// \param[in] scale The scale factor for the text.
+          /// \param[in] align_horizontal The horizontal alignment of the text.
+          /// \param[in] align_vertical The vertical alignment of the text.
+          ///
+          /// \pre There is an initialized canvas, text color is initialized and
+          ///      any necessary rotation has been done.
+          ///
+          /// \post The text is rendered at the specified location, with the 
+          ///       specified alignments and at the specified scale. It is cropped
+          ///       To fit within the bounding box defined by xLft, xRgt, yBot and
+          ///       yTop.
+          void draw_cropped_text(
+            const std::string s,
+            const double x,
+            const double y,
+            const double xLft,
+            const double xRgt,
+            const double yTop,
+            const double yBot,
+            const double scale,
+            const Alignment align_horizontal,
+            const Alignment align_vertical
+          );
+
+          /// \brief Renders the string s wrapped into a bounding box.
+          /// \param[in] s The string to be drawn.
+          /// \param[in] xLft The left side of the bounding box.
+          /// \param[in] xRgt The right side of the bounding box.
+          /// \param[in] yBot The bottom of the bounding box.
+          /// \param[in] yTop The top of the bounding box.
+          /// \param[in] scale The scale factor for the text.
+          /// \param[in] align_horizontal The horizontal alignment of the text.
+          /// \param[in] align_vertical The vertical alignment of the text.
+          ///
+          /// \pre There is an initialized canvas, text color is initialized and
+          ///      any necessary rotation has been done.
+          ///
+          /// \post The text is rendered at the specified location, with the 
+          ///       specified alignments and at the specified scale. It is cropped
+          ///       To fit within the bounding box defined by xLft, xRgt, yBot and
+          ///       yTop.
+          void draw_wrapped_text(
+            const std::string s,
+            const double xLft, 
+            const double xRgt,
+            const double yTop, 
+            const double yBot,
+            const double scale,
+            const Alignment align_horizontal,
+            const Alignment align_vertical
+          );
+
+          private:
+            static GLuint  (&character_texture_id())[CHARSETSIZE];
+            static GLubyte (&character_textures())[CHARSETSIZE][CHARHEIGHT * CHARWIDTH];
+
+            size_t index_from_char(const char & c);
+
+            bool initialise();
+      }; // class font_renderer
+    } // namespace wx
   } // namespace utilities
 } // namespace mcrl2
 

libraries/utilities/include/mcrl2/utilities/spinctrlfloat.h
@@ -18,62 +18,64 @@
 
 namespace mcrl2 {
   namespace utilities {
+    namespace wx {
 
-    /**
-     * \brief A spin control widget
-     *
-     * A widget that consists of a text field with a string that represents a
-     * numeric value in it that is editable by the user, and a pair of buttons
-     * that can be used to increase or decrease the value in the text field
-     * with a configurable amount.
-     **/
-    class wxSpinCtrlFloat : public wxPanel {
+      /**
+       * \brief A spin control widget
+       *
+       * A widget that consists of a text field with a string that represents a
+       * numeric value in it that is editable by the user, and a pair of buttons
+       * that can be used to increase or decrease the value in the text field
+       * with a configurable amount.
+       **/
+      class wxSpinCtrlFloat : public wxPanel {
 
-      private:
+        private:
 
-        /// \brief A wxWidgets text control, used to display the current value
-        wxTextCtrl*   m_TextCtrl;
-        /// \brief A wxWidgets spin button, used to manipulate the current value
-        wxSpinButton* m_SpinButton;
-    	
-        /// \brief The spin button resolution
-        double m_Rate;
+          /// \brief A wxWidgets text control, used to display the current value
+          wxTextCtrl*   m_TextCtrl;
+          /// \brief A wxWidgets spin button, used to manipulate the current value
+          wxSpinButton* m_SpinButton;
 
-        /// \brief The rounded minimum value
-        int    m_Min;
+          /// \brief The spin button resolution
+          double m_Rate;
 
-        /// \brief The rounded maximum value
-        int    m_Max;
-          
-        DECLARE_EVENT_TABLE()
+          /// \brief The rounded minimum value
+          int    m_Min;
 
-      private:
+          /// \brief The rounded maximum value
+          int    m_Max;
 
-        /// \brief Event handler for SPIN events
-        void OnEnter(wxCommandEvent & event);
+          DECLARE_EVENT_TABLE()
 
-        /// \brief Event handler for SPIN events
-        void OnSpin(wxSpinEvent & event);
+        private:
 
-      public:
+          /// \brief Event handler for SPIN events
+          void OnEnter(wxCommandEvent & event);
 
-        /// \brief Constructor
-        wxSpinCtrlFloat(
-            wxWindow* parent, 
-            wxWindowID id, 
-            double p_Min, 
-            double p_Max,
-            double p_Rate, 
-            double p_Init, 
-            const wxPoint& pos = wxDefaultPosition,
-            const wxSize& size = wxDefaultSize );
-    
-        /// \brief Gets current value
-        double GetValue();
+          /// \brief Event handler for SPIN events
+          void OnSpin(wxSpinEvent & event);
 
-        /// \brief Sets current value
-        void SetValue(float v);
-    };
+        public:
+
+          /// \brief Constructor
+          wxSpinCtrlFloat(
+              wxWindow* parent, 
+              wxWindowID id, 
+              double p_Min, 
+              double p_Max,
+              double p_Rate, 
+              double p_Init, 
+              const wxPoint& pos = wxDefaultPosition,
+              const wxSize& size = wxDefaultSize );
+
+          /// \brief Gets current value
+          double GetValue();
+
+          /// \brief Sets current value
+          void SetValue(float v);
+      };
+    }
   }
 }
 

libraries/utilities/include/mcrl2/utilities/wx_tool.h
@@ -25,6 +25,9 @@
 
 namespace mcrl2 {
   namespace utilities {
+    /**
+     * \brief Namespace containing wxWidgets utility functionality
+     **/
     namespace wx {
       /**
        * \brief Component for building wxWidgets tools with mCRL2 toolset command line interface

commit 0cf6be30a835887a83f42deb31335db58a3e0c0d

Changes:
 - fix for building in non-debug mode (caused by r5448 and r5449)



git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5453 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/core/source/struct.cpp
@@ -41,13 +41,11 @@ static bool IsPNSort(ATermAppl SortExpr);
 static bool IsPNISort(ATermAppl SortExpr);
 //Ret: SortExpr is a sort expression for Pos, Nat or Int
 
-#ifndef NDEBUG
 static bool IsNISort(ATermAppl SortExpr);
 //Ret: SortExpr is a sort expression for Nat or Int
 
 static bool IsPNIRSort(ATermAppl SortExpr);
 //Ret: SortExpr is a sort expression for Pos, Nat, Int or Real
-#endif
 
 static ATermAppl IntersectPNSorts(ATermAppl SortExpr1, ATermAppl SortExpr2);
 //Pre: IsPNSort(SortExpr1) and IsPNSort(SortExpr2)
@@ -4755,7 +4753,6 @@ bool IsPNISort(ATermAppl SortExpr)
     ATisEqual(SortExpr, gsMakeSortExprInt());
 }
 
-#ifndef NDEBUG
 bool IsNISort(ATermAppl SortExpr)
 {
   return
@@ -4769,7 +4766,6 @@ bool IsPNIRSort(ATermAppl SortExpr)
     IsPNISort(SortExpr) ||
     ATisEqual(SortExpr, gsMakeSortExprReal());
 }
-#endif
 
 ATermAppl IntersectPNSorts(ATermAppl SortExpr1, ATermAppl SortExpr2)
 {