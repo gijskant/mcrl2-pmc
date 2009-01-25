gijskant/mcrl2-pmc

commit fae9e53a2a3ae25ed366d609ea56540cb52ca9a9

- Perform type checking of data expressions and sort expressions against a   specification in the internal format before data implementation.
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@4305 21252fe5-60e8-0310-8ff0-c1ae1001594e

tools/lpsinvelm/lpsinvelm.cpp
@@ -17,6 +17,7 @@
 #include "mcrl2/core/detail/parse.h"
 #include "mcrl2/core/detail/typecheck.h"
 #include "mcrl2/core/detail/data_implementation.h"
+#include "mcrl2/core/detail/data_reconstruct.h"
 #include "mcrl2/data/prover/bdd_path_eliminator.h"
 #include "mcrl2/lps/linear_process.h"
 #include "mcrl2/lps/specification.h"
@@ -423,8 +424,9 @@ using namespace mcrl2;
         exit(1);
       }
 
-      // temporary measure until the invariant and confluence checkers use the lps framework
-      f_lps = (ATermAppl) lps_specification;
+      // type checking and data implementation of data expressions use an lps
+      // before data implementation
+      ATermAppl f_reconstructed_spec = reconstruct_spec(lps_specification);      
 
       //parse the invariant formula from infilename
       std::ifstream instream(f_invariant_file_name);
@@ -440,13 +442,13 @@ using namespace mcrl2;
       }
       
       //typecheck the invariant formula
-      ATermList vars = lps_specification.process().process_parameters();
+      ATermList vars = ATLgetArgument(ATAgetArgument(ATAgetArgument(f_reconstructed_spec, 2), 0), 0);
       ATermTable var_table = ATtableCreate(63,50);
       for (; !ATisEmpty(vars); vars = ATgetNext(vars)) {
         ATermAppl var = ATAgetFirst(vars);
         ATtablePut(var_table, ATgetArgument(var, 0), ATgetArgument(var, 1));
       } 
-      f_invariant = type_check_data_expr(f_invariant, gsMakeSortIdBool(), lps_specification, var_table);
+      f_invariant = type_check_data_expr(f_invariant, gsMakeSortIdBool(), f_reconstructed_spec, var_table);
       ATtableDestroy(var_table);
       if(!f_invariant){
         gsErrorMsg("Typechecking of the invariant formula failed.\n");
@@ -454,11 +456,13 @@ using namespace mcrl2;
       }
      
       //data implement the invariant formula
-      f_invariant = implement_data_data_expr(f_invariant,lps_specification);
+      f_invariant = implement_data_data_expr(f_invariant,f_reconstructed_spec);
       if(!f_invariant){
         gsErrorMsg("Data implementation of the invariant formula failed.\n");
         exit(1);
       }
+
+      f_lps = lps::specification(f_reconstructed_spec);
     }
 
     // --------------------------------------------------------------------------------------------

libraries/core/include/mcrl2/core/detail/data_implementation.h
@@ -50,32 +50,33 @@ ATermAppl implement_data_pbes_spec(ATermAppl spec);
 
 /** \pre sort_expr represents a sort expression that adheres to the internal
  *     ATerm structure after the type checking phase.
- *     lps_spec represents an LPS specification
+ *     spec represents an LPS specification, a PBES or a data specification in
+ *     the internal format before data implementation.
  * \post The datatypes of sort_expr are implemented as higher-order abstract data
- *     types in lps_spec
+ *     types in spec
+ *     spec is updated to be the equivalent of spec in the internal format after
+ *     data implementation.
  * \return if the data implementation went well, an equivalent version of sort_expr
  *     is returned that adheres to the internal ATerm structure after data
  *     implementation.
  *     If something went wrong, an appropriate error message is printed and
  *     NULL is returned.
  **/
-ATermAppl implement_data_sort_expr(ATermAppl sort_expr,
-  lps::specification &lps_spec);
+ATermAppl implement_data_sort_expr(ATermAppl sort_expr, ATermAppl& spec);
 
 
 /** \pre data_expr represents a data expression that adheres to the internal
  *     ATerm structure after the type checking phase.
- *     lps_spec represents an LPS specification
- * \post The datatypes of data_expr are implemented as higher-order abstract data
- *     types in lps_spec
+ *     spec represents an LPS specification before data implementation
+ * \post The datatypes of spec are implemented as higher-order abstract data
+ *     types.
  * \return if the data implementation went well, an equivalent version of data_expr
  *     is returned that adheres to the internal ATerm structure after data
  *     implementation.
  *     If something went wrong, an appropriate error message is printed and
  *     NULL is returned.
  **/
-ATermAppl implement_data_data_expr(ATermAppl data_expr,
-  lps::specification &lps_spec);
+ATermAppl implement_data_data_expr(ATermAppl data_expr, ATermAppl& spec);
 
 
 /** \pre mult_act represents a multiaction that adheres to the internal ATerm
@@ -120,8 +121,7 @@ ATermAppl implement_data_proc_expr(ATermAppl proc_expr,
  *     If something went wrong, an appropriate error message is printed and
  *     NULL is returned.
  **/
-ATermAppl implement_data_state_frm(ATermAppl state_frm,
-  ATermAppl& spec);
+ATermAppl implement_data_state_frm(ATermAppl state_frm, ATermAppl& spec);
 
 
 /** \pre ar_spec represents an action rename specification that

libraries/core/include/mcrl2/core/detail/typecheck.h
@@ -34,14 +34,15 @@ ATermAppl type_check_proc_spec(ATermAppl proc_spec);
 
 /** \pre  sort_expr represents an mCRL2 sort expression that adheres to the
  *      initial internal ATerm structure.
- *      lps_spec represents an LPS specification
+ *      spec represents an LPS specification, a PBES or a data specification
+ *      in the internal format before data implementation.
  * \post sort_expr is type checked using the declaration from lps_spec
  * \return  if type checking went well, an equivalent version of sort_expr is
  *      returned that adheres to the internal ATerm structure after type
  *      checking. if something went wrong, an appropriate error message is
  *      printed and NULL is returned.
  **/
-ATermAppl type_check_sort_expr(ATermAppl sort_expr, lps::specification &lps_spec);
+ATermAppl type_check_sort_expr(ATermAppl sort_expr, ATermAppl spec);
 
 
 /** \pre  sort_expr represents an mCRL2 sort expression that adheres to the
@@ -61,7 +62,8 @@ ATermAppl type_check_sort_expr_part(ATermAppl sort_expr, ATermAppl spec=NULL);
  *      initial internal ATerm structure.
  *      sort_expr represents an mCRL2 sort expression that adheres to the
  *      internal ATerm structure after type checking or is NULL.
- *      lps_spec represents an LPS specification
+ *      spec represents an LPS specification, a PBES or a data specification in
+ *      the format before data implementation.
  *      Vars contains any extra variables that can be used in the data expression
  * \post data_expr is type checked using the declaration from lps_spec and, if
  *      sort_expr is not NULL, it is type checked as being of type sort_expr
@@ -70,7 +72,7 @@ ATermAppl type_check_sort_expr_part(ATermAppl sort_expr, ATermAppl spec=NULL);
  *      if something went wrong, an appropriate error message is printed and
  *      NULL is returned.
  **/
-ATermAppl type_check_data_expr(ATermAppl data_expr, ATermAppl sort_expr, lps::specification &lps_spec, ATermTable Vars=NULL);
+ATermAppl type_check_data_expr(ATermAppl data_expr, ATermAppl sort_expr, ATermAppl spec, ATermTable Vars=NULL);
 
 
 /** \pre  data_expr represents an mCRL2 data expression that adheres to the

libraries/core/source/data_implementation.cpp
@@ -87,6 +87,20 @@ static void impl_standard_functions_term(ATerm term, lps::specification &lps_spe
 //Post:an implementation for equality, inequality and if
 //     is added to lps_spec for each sort occurring in term.
 
+static ATermAppl impl_exprs_with_spec(ATermAppl part, ATermAppl& spec);
+//Pre: part is an expression that adheres to the internal syntax after type
+//     checking.
+//     spec represents an LPS specification in the internal syntax after type
+//     checking.
+//Post: The datatypes of spec are implemented as higher-order abstract data
+//     types in spec
+//Ret: if the data implementation went well, an equivalent version of part
+//     is returned that adheres to the internal ATerm structure after data
+//     implementation.
+//     If something went wrong, an appropriate error message is printed and
+//     NULL is returned.
+
+
 static ATermAppl impl_exprs_appl(ATermAppl part, ATermList *p_substs,
   t_data_decls *p_data_decls);
 //Pre: part is a part of a specification that adheres to the internal syntax
@@ -209,16 +223,14 @@ ATermAppl implement_data_proc_spec(ATermAppl spec)
   return spec;
 }
 
-ATermAppl implement_data_sort_expr(ATermAppl sort_expr,
-  lps::specification &lps_spec)
+ATermAppl implement_data_sort_expr(ATermAppl sort_expr, ATermAppl& spec)
 {
-  return impl_exprs(sort_expr, lps_spec);
+  return impl_exprs_with_spec(sort_expr, spec);
 }
 
-ATermAppl implement_data_data_expr(ATermAppl data_expr,
-  lps::specification &lps_spec)
+ATermAppl implement_data_data_expr(ATermAppl data_expr, ATermAppl& spec)
 {
-  return impl_exprs(data_expr, lps_spec);
+  return impl_exprs_with_spec(data_expr, spec);
 }
 
 ATermAppl implement_data_mult_act(ATermAppl mult_act,
@@ -235,43 +247,7 @@ ATermAppl implement_data_proc_expr(ATermAppl proc_expr,
 
 ATermAppl implement_data_state_frm(ATermAppl state_frm, ATermAppl& spec)
 {
-  assert(gsIsSpecV1(spec) || gsIsPBES(spec) || gsIsDataSpec(spec));
-  int occ =
-    gsCount((ATerm) gsMakeSortUnknown(), (ATerm) spec) +
-    gsCountAFun(gsAFunSortsPossible(), (ATerm) spec);
-  if (occ > 0) {
-    gsErrorMsg("specification contains %d unresolved type%s\n", occ, (occ != 1)?"s":"");
-    return NULL;
-  }
-  occ = gsCount((ATerm) gsMakeSortUnknown(), (ATerm) state_frm);
-  if (occ > 0) {
-    gsErrorMsg("state formula contains %d unknown type%s\n", occ, (occ != 1)?"s":"");
-    return NULL;
-  }
-
-  //implement system sorts and data expressions occurring in spec
-  ATermList substs     = ATmakeList0();
-  t_data_decls data_decls;
-  initialize_data_decls(&data_decls);
-  spec = impl_exprs_appl(spec, &substs, &data_decls);
-  state_frm = impl_exprs_appl(state_frm, &substs, &data_decls);
-  //perform substitutions on data declarations
-  data_decls.sorts     = gsSubstValues_List(substs, data_decls.sorts,     true);
-  data_decls.cons_ops  = gsSubstValues_List(substs, data_decls.cons_ops,  true);
-  data_decls.ops       = gsSubstValues_List(substs, data_decls.ops,       true);
-  data_decls.data_eqns = gsSubstValues_List(substs, data_decls.data_eqns, true);
-  //add implementation of sort Pos and Bool
-  impl_sort_pos(&data_decls);
-  impl_sort_bool(&data_decls);
-  //add new data declarations to spec
-  spec = add_data_decls(spec, data_decls);
-  //implement numerical pattern matching
-  spec = impl_numerical_pattern_matching(spec);
-  //implement sort references
-  spec = impl_sort_refs(spec);
-  //implement standard functions
-  spec = impl_standard_functions_spec(spec);
-  return state_frm;
+  return impl_exprs_with_spec(state_frm, spec);
 }
 
 ATermAppl implement_data_action_rename_spec(ATermAppl ar_spec,
@@ -525,6 +501,47 @@ ATermAppl impl_numerical_pattern_matching_expr(ATermAppl data_expr, bool top_lev
   }
   args = ATreverse(l);
   return gsMakeDataAppl(head, args);
+}
+
+ATermAppl impl_exprs_with_spec(ATermAppl part, ATermAppl& spec)
+{
+  assert(gsIsSpecV1(spec) || gsIsPBES(spec) || gsIsDataSpec(spec));
+  int occ =
+    gsCount((ATerm) gsMakeSortUnknown(), (ATerm) spec) +
+    gsCountAFun(gsAFunSortsPossible(), (ATerm) spec);
+  if (occ > 0) {
+    gsErrorMsg("specification contains %d unresolved type%s\n", occ, (occ != 1)?"s":"");
+    return NULL;
+  }
+  occ = gsCount((ATerm) gsMakeSortUnknown(), (ATerm) part);
+  if (occ > 0) {
+    gsErrorMsg("part contains %d unknown type%s\n", occ, (occ != 1)?"s":"");
+    return NULL;
+  }
+
+  //implement system sorts and data expressions occurring in spec
+  ATermList substs     = ATmakeList0();
+  t_data_decls data_decls;
+  initialize_data_decls(&data_decls);
+  spec = impl_exprs_appl(spec, &substs, &data_decls);
+  part = impl_exprs_appl(part, &substs, &data_decls);
+  //perform substitutions on data declarations
+  data_decls.sorts     = gsSubstValues_List(substs, data_decls.sorts,     true);
+  data_decls.cons_ops  = gsSubstValues_List(substs, data_decls.cons_ops,  true);
+  data_decls.ops       = gsSubstValues_List(substs, data_decls.ops,       true);
+  data_decls.data_eqns = gsSubstValues_List(substs, data_decls.data_eqns, true);
+  //add implementation of sort Pos and Bool
+  impl_sort_pos(&data_decls);
+  impl_sort_bool(&data_decls);
+  //add new data declarations to spec
+  spec = add_data_decls(spec, data_decls);
+  //implement numerical pattern matching
+  spec = impl_numerical_pattern_matching(spec);
+  //implement sort references
+  spec = impl_sort_refs(spec);
+  //implement standard functions
+  spec = impl_standard_functions_spec(spec);
+  return part;
 }
 
 ATermAppl impl_exprs_appl(ATermAppl part, ATermList *p_substs,

libraries/core/source/typecheck.cpp
@@ -305,11 +305,12 @@ ATermAppl type_check_data_expr_part(ATermAppl data_expr, ATermAppl sort_expr, AT
   return Result;
 }
 
-ATermAppl type_check_sort_expr(ATermAppl sort_expr, lps::specification &lps_spec)
+ATermAppl type_check_sort_expr(ATermAppl sort_expr, ATermAppl spec)
 {
   //check correctness of the sort expression in sort_expr using
   //the LPS specification in lps_spec
   assert(gsIsSortExpr(sort_expr));
+  assert(gsIsSpecV1(spec) || gsIsPBES(spec) || gsIsDataSpec(spec));
   //gsWarningMsg("type checking of sort expressions is partially implemented\n");
   
   ATermAppl Result=NULL;
@@ -320,8 +321,19 @@ ATermAppl type_check_sort_expr(ATermAppl sort_expr, lps::specification &lps_spec
 
   gsDebugMsg ("type checking of sort expressions read-in phase started\n");
 
+  ATermAppl data_spec = NULL;
+  if (gsIsSpecV1(spec) || gsIsPBES(spec))
+  {
+    data_spec = ATAgetArgument(spec, 0);
+  }
+  else
+  {
+    data_spec = spec;
+  }
+  ATermList sorts = ATLgetArgument(ATAgetArgument(data_spec, 0), 0);
+
   //XXX read-in from LPS (not finished)
-  if(gstcReadInSorts((ATermList) lps_spec.data().sorts(),false)){
+  if(gstcReadInSorts(sorts,false)){
     gsDebugMsg ("type checking of sort expressions read-in phase finished\n");
 
     if(!gsIsNotInferred(sort_expr)){
@@ -339,12 +351,13 @@ ATermAppl type_check_sort_expr(ATermAppl sort_expr, lps::specification &lps_spec
   return Result;
 }
 
-ATermAppl type_check_data_expr(ATermAppl data_expr, ATermAppl sort_expr, lps::specification &lps_spec, ATermTable Vars)
+ATermAppl type_check_data_expr(ATermAppl data_expr, ATermAppl sort_expr, ATermAppl spec, ATermTable Vars)
 {
   //check correctness of the data expression in data_expr using
-  //the LPS specification in lps_spec
+  //the LPS specification in spec
   
   //check preconditions
+  assert(gsIsSpecV1(spec) || gsIsPBES(spec) || gsIsDataSpec(spec));
   assert(gsIsDataExpr(data_expr));
   assert((sort_expr == NULL) || gsIsSortExpr(sort_expr));
 
@@ -356,10 +369,23 @@ ATermAppl type_check_data_expr(ATermAppl data_expr, ATermAppl sort_expr, lps::sp
 
   gsDebugMsg ("type checking of data expression read-in phase started\n");
 
+  ATermAppl data_spec = NULL;
+  if (gsIsSpecV1(spec) || gsIsPBES(spec))
+  {
+    data_spec = ATAgetArgument(spec, 0);
+  }
+  else
+  {
+    data_spec = spec;
+  }
+  ATermList sorts = ATLgetArgument(ATAgetArgument(data_spec, 0), 0);
+  ATermList constructors = ATLgetArgument(ATAgetArgument(data_spec, 1), 0);
+  ATermList mappings = ATLgetArgument(ATAgetArgument(data_spec, 2), 0);
+
   //XXX read-in from LPS (not finished)
-  if (gstcReadInSorts((ATermList) lps_spec.data().sorts(),false) &&
+  if (gstcReadInSorts(sorts,false) &&
       gstcReadInConstructors() &&
-      gstcReadInFuncs(ATconcat((ATermList) lps_spec.data().constructors(),(ATermList) lps_spec.data().mappings()),false))
+      gstcReadInFuncs(ATconcat(constructors,mappings),false))
   {
     gsDebugMsg ("type checking of data expression read-in phase finished\n");
 
@@ -378,6 +404,7 @@ ATermAppl type_check_data_expr(ATermAppl data_expr, ATermAppl sort_expr, lps::sp
     gsErrorMsg("reading from LPS failed\n");
   }
   gstcDataDestroy();
+
   return Result;
 }
 

libraries/lts/source/liblts_fsm.cpp
@@ -17,6 +17,7 @@
 #include "mcrl2/core/detail/parse.h"
 #include "mcrl2/core/detail/typecheck.h"
 #include "mcrl2/core/detail/data_implementation.h"
+#include "mcrl2/core/detail/data_reconstruct.h"
 #include "mcrl2/core/print.h"
 #include "mcrl2/lts/liblts.h"
 #include "mcrl2/lps/specification.h"
@@ -73,6 +74,10 @@ static ATerm parse_mcrl2_state(ATerm state, lps::specification &spec)
     ATermAppl expr = ATAgetArgument(val,0);
     ATermAppl sort = ATAgetArgument(ATAgetArgument(val,1),1);
 
+    // typechecking and data implementation of terms needs an lps
+    // before data implementation
+    ATermAppl reconstructed_lps = reconstruct_spec(spec);
+
     std::stringstream sort_ss(ATgetName(ATgetAFun(sort)));
     sort = parse_sort_expr(sort_ss);
     if ( sort == NULL )
@@ -80,7 +85,7 @@ static ATerm parse_mcrl2_state(ATerm state, lps::specification &spec)
       gsVerboseMsg("error parsing state argument sort\n");
       return NULL;
     }
-    sort = type_check_sort_expr(sort,spec);
+    sort = type_check_sort_expr(sort,reconstructed_lps);
     if ( sort == NULL )
     {
       gsVerboseMsg("error type checking state argument sort\n");
@@ -94,18 +99,20 @@ static ATerm parse_mcrl2_state(ATerm state, lps::specification &spec)
       gsVerboseMsg("error parsing state argument\n");
       return NULL;
     }
-    expr = type_check_data_expr(expr,sort,spec);
+    expr = type_check_data_expr(expr,sort,reconstructed_lps);
     if ( expr == NULL )
     {
       gsVerboseMsg("error type checking state argument\n");
       return NULL;
     }
-    expr = implement_data_data_expr(expr,spec);
+    expr = implement_data_data_expr(expr,reconstructed_lps);
     if ( expr == NULL )
     {
       gsVerboseMsg("error implementing data of state argument\n");
       return NULL;
     }
+
+    spec = lps::specification(reconstructed_lps);
 
     state_args[i] = (ATerm) expr; 
   }

tools/lpsconfcheck/lpsconfcheck.cpp
@@ -16,6 +16,7 @@
 #include "mcrl2/core/detail/parse.h"
 #include "mcrl2/core/detail/typecheck.h"
 #include "mcrl2/core/detail/data_implementation.h"
+#include "mcrl2/core/detail/data_reconstruct.h"
 #include "mcrl2/core/messaging.h"
 #include "mcrl2/core/struct.h"
 #include "mcrl2/utilities/aterm_ext.h"
@@ -696,25 +697,31 @@ bool squadt_interactor::perform_task(tipi::configuration& c) {
       }
       // temporary measure, until the invariant and confluence checkers use the lps framework
       f_lps = (ATermAppl) lps_specification;
+        // type checking and data implementation of data expressions use an lps
+        // before data implementation
+      ATermAppl f_reconstructed_lps = reconstruct_spec(f_lps);
 
       //typecheck the invariant formula
-      ATermList vars = lps_specification.process().process_parameters();
+      ATermList vars =  ATLgetArgument(ATAgetArgument(ATAgetArgument(f_reconstructed_lps, 2), 0), 0);
       ATermTable var_table = ATtableCreate(63,50);
       for (; !ATisEmpty(vars); vars = ATgetNext(vars)) {
         ATermAppl var = ATAgetFirst(vars);
         ATtablePut(var_table, ATgetArgument(var, 0), ATgetArgument(var, 1));
       }
-      f_invariant = type_check_data_expr(f_invariant, gsMakeSortIdBool(), lps_specification, var_table);
+      f_invariant = type_check_data_expr(f_invariant, gsMakeSortIdBool(), f_reconstructed_lps, var_table);
       ATtableDestroy(var_table);
       if (!f_invariant) {
         throw std::runtime_error("Typechecking of the invariant formula failed.\n");
       }
      
       //data implement the invariant formula
-      f_invariant = implement_data_data_expr(f_invariant, lps_specification);
+      f_invariant = implement_data_data_expr(f_invariant, f_reconstructed_lps);
       if (!f_invariant) {
         throw std::runtime_error("Data implementation of the invariant formula failed.\n");
       }
+
+      f_lps = specification(f_reconstructed_lps);
+
     }
 
     // --------------------------------------------------------------------------------------------

tools/lpsformcheck/lpsformcheck.cpp
@@ -19,6 +19,7 @@
 #include "mcrl2/core/detail/parse.h"
 #include "mcrl2/core/detail/typecheck.h"
 #include "mcrl2/core/detail/data_implementation.h"
+#include "mcrl2/core/detail/data_reconstruct.h"
 #include "mcrl2/utilities/aterm_ext.h"
 #include "mcrl2/utilities/version_info.h"
 #include <string>
@@ -320,8 +321,10 @@ using namespace mcrl2;
       if (!lps_specification.is_well_typed()) {
         throw std::runtime_error("invalid mCRL2 LPS read from " + std::string((f_lps_file_name == 0)?"stdin":f_lps_file_name));
       }
-      // temporary measure, until the invariant and confluence checkers use the lps framework
-      ATermAppl v_lps = (ATermAppl) lps_specification;
+
+      // typechecking and data implementation use a specification before data
+      // implementation.
+      ATermAppl v_reconstructed_lps = reconstruct_spec(lps_specification);
 
       gsVerboseMsg("parsing formula file '%s'...\n", f_formula_file_name);
       //open the formula from f_formula_file_name
@@ -336,19 +339,19 @@ using namespace mcrl2;
         throw std::runtime_error("cannot parse formula from '" + std::string(f_formula_file_name) + "'");
       }
       //typecheck the formula
-      f_formula = type_check_data_expr(f_formula, gsMakeSortIdBool(), lps_specification);
+      f_formula = type_check_data_expr(f_formula, gsMakeSortIdBool(), v_reconstructed_lps);
       if(!f_formula){
         throw std::runtime_error("type checking formula from '" + std::string(f_formula_file_name) + "' failed");
       }
       //implement data in the formula
-      f_formula = implement_data_data_expr(f_formula,lps_specification);
+      f_formula = implement_data_data_expr(f_formula,v_reconstructed_lps);
       if(!f_formula){
         throw std::runtime_error("implementation of data types in the formula from '" + std::string(f_formula_file_name) + "' failed");
       }
 
       //check formula
       Formula_Checker v_formula_checker(
-        ATAgetArgument(v_lps,0), f_strategy, f_time_limit, f_path_eliminator, f_solver_type, f_apply_induction, f_counter_example, f_witness, f_dot_file_name);
+        ATAgetArgument(v_reconstructed_lps,0), f_strategy, f_time_limit, f_path_eliminator, f_solver_type, f_apply_induction, f_counter_example, f_witness, f_dot_file_name);
       v_formula_checker.check_formulas(ATmakeList1((ATerm) f_formula));
     }
 

commit d453e4343503c8079df803250eae8152982306ad

Bugfix in lpsinvelm: it was not possible to use LPS process parameters in the supplied invariant.
The issue was introduced in revision r4305 (and was almost fixed in revision r4526).

This commit resolves ticket #555.

Reviewed by Jan Friso Groote.


git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5539 21252fe5-60e8-0310-8ff0-c1ae1001594e

tools/lpsinvelm/lpsinvelm.cpp
@@ -286,7 +286,7 @@ using namespace mcrl2::core;
       }
       
       //typecheck the invariant formula
-      ATermList vars = ATLgetArgument(ATAgetArgument(f_reconstructed_spec, 2), 0);
+      ATermList vars = ATLgetArgument(ATAgetArgument(f_reconstructed_spec, 2), 1);
       ATermTable var_table = ATtableCreate(63,50);
       for (; !ATisEmpty(vars); vars = ATgetNext(vars)) {
         ATermAppl var = ATAgetFirst(vars);