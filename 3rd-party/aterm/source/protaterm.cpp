gijskant/mcrl2-pmc

commit 70f91ea34d37627bdfda1d120e78b555f4524462

Replaced array of protected structures with ATerms (e.g. atermpp::vector) by a multiset. This has a significant impact on lpsrealelm, which heavily uses such structures.
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5699 21252fe5-60e8-0310-8ff0-c1ae1001594e

3rd-party/aterm/source/protaterm.cpp
@@ -1,51 +1,29 @@
-#include <stdlib.h>
+#include <set>
 #include "protaterm.h"
+
+using namespace std;
 
 IProtectedATerm::~IProtectedATerm()
 {
 }
 
-IProtectedATerm **p_aterms = NULL;
-int p_aterms_size = 0;
-int p_aterms_count = 0;
+typedef multiset<IProtectedATerm *> pa_container;
+static pa_container p_aterms;
 
 void AT_protectProtectedATerms()
 {
-  int i;
-  for (i=0; i<p_aterms_count; i++)
+  for (pa_container::iterator i=p_aterms.begin(); i!=p_aterms.end(); i++)
   {
-    p_aterms[i]->ATprotectTerms();
+    (*i)->ATprotectTerms();
   }
 }
 
 void ATprotectProtectedATerm(IProtectedATerm *i)
 {
-  if ( p_aterms_count == p_aterms_size )
-  {
-    if ( p_aterms_size == 0 )
-    {
-      p_aterms_size = 32;
-      ATaddProtectFunction(AT_protectProtectedATerms);
-    } else {
-      p_aterms_size = p_aterms_size * 2;
-    }
-
-    p_aterms = (IProtectedATerm **) realloc(p_aterms, p_aterms_size*sizeof(IProtectedATerm *));
-  }
-
-  p_aterms[p_aterms_count] = i;
-  p_aterms_count++;
+  p_aterms.insert(i);
 }
 
 void ATunprotectProtectedATerm(IProtectedATerm *i)
 {
-  int j;
-  for (j=0; j<p_aterms_count; j++)
-  {
-    if ( p_aterms[j] == i )
-    {
-      p_aterms_count--;
-      p_aterms[j] = p_aterms[p_aterms_count];
-    }
-  }
+  p_aterms.erase(p_aterms.find(i));
 }

commit 38f64b30dc4f419efcc813cfce5265405211fd8e

Fixed bug introduced in r5699; I mistakenly removed the registration of a function that is needed to protect ATerms in atermpp containers.
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@5701 21252fe5-60e8-0310-8ff0-c1ae1001594e

3rd-party/aterm/source/protaterm.cpp
@@ -8,9 +8,17 @@ IProtectedATerm::~IProtectedATerm()
 }
 
 typedef multiset<IProtectedATerm *> pa_container;
-static pa_container p_aterms;
 
-void AT_protectProtectedATerms()
+static void AT_protectProtectedATerms();
+
+static pa_container initialise_p_aterms()
+{
+  ATaddProtectFunction(AT_protectProtectedATerms);
+  return pa_container();
+}
+static pa_container p_aterms = initialise_p_aterms();
+
+static void AT_protectProtectedATerms()
 {
   for (pa_container::iterator i=p_aterms.begin(); i!=p_aterms.end(); i++)
   {