gijskant/mcrl2-pmc

commit ee13fdd2af51d2250d1253b2d2a60d18f77ca0fc

- Moved global variables, together with Aad.
N.B. There are still some problems:

- Many programs fail during type checking. The internal term structure of the data types is hard coded in the type checker, so probably moving global variables has introduced some errors.

- The tool lps_action_rename must be repaired. This is related to the function rename_global_variables in lps/rename.h. This one should be easy to fix.

- The file bisimulation.h must be repaired w.r.t. the changes to global variables.

- A few tools still need to be adapted (lpsrealelm, lpsparunfold).

- The status of the PBES library is unknown.



git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6602 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/core/source/typecheck.cpp
@@ -76,7 +76,7 @@ static ATbool gstcReadInConstructors(ATermList NewSorts=NULL);
 static ATbool gstcReadInFuncs(ATermList, ATermList, bool high_level=true);
 static ATbool gstcReadInActs (ATermList);
 static ATbool gstcReadInProcsAndInit (ATermList, ATermAppl);
-static ATbool gstcReadInPBESAndInit(ATermAppl, ATermAppl);
+static ATbool gstcReadInPBESAndInit(ATermAppl, ATermAppl, ATermAppl);
 
 static ATbool gstcTransformVarConsTypeData(void);
 static ATbool gstcTransformActProcVarConst(void);
@@ -278,8 +278,14 @@ ATermAppl type_check_proc_spec(ATermAppl proc_spec)
 		     ATLgetArgument(ATAgetArgument(data_spec,2),0))) {
   body.equations=ATLgetArgument(ATAgetArgument(data_spec,3),0);
   if(gstcReadInActs(ATLgetArgument(ATAgetArgument(proc_spec,1),0))) {
-  if(gstcReadInProcsAndInit(ATLgetArgument(ATAgetArgument(proc_spec,2),0),
-			     ATAgetArgument(ATAgetArgument(proc_spec,3),1))) {
+  // XXX To do: implement type checking of global variable declaration
+  ATermAppl glob_var_spec = ATAgetArgument(proc_spec,2);
+  ATermList glob_vars = ATLgetArgument(glob_var_spec,0);
+  if(!ATisEmpty(glob_vars)) {
+    gsWarningMsg("type checking of global variable declarations is not implemented yet\n");
+  }
+  if(gstcReadInProcsAndInit(ATLgetArgument(ATAgetArgument(proc_spec,3),0),
+			     ATAgetArgument(ATAgetArgument(proc_spec,4),0))) {
   gsDebugMsg ("type checking read-in phase finished\n");
 
   gsDebugMsg ("type checking transform ActProc+VarConst phase started\n");
@@ -290,9 +296,8 @@ ATermAppl type_check_proc_spec(ATermAppl proc_spec)
   data_spec=ATAgetArgument(proc_spec,0);
   data_spec=ATsetArgument(data_spec, (ATerm) gsMakeDataEqnSpec(body.equations),3);
   Result=ATsetArgument(proc_spec,(ATerm)data_spec,0);
-  Result=ATsetArgument(Result,(ATerm)gsMakeProcEqnSpec(gstcWriteProcs(ATLgetArgument(ATAgetArgument(proc_spec,2),0))),2);
-  Result=ATsetArgument(Result,(ATerm)gsMakeProcessInit(ATmakeList0(),
-    ATAtableGet(body.proc_bodies,(ATerm)INIT_KEY())),3);
+  Result=ATsetArgument(Result,(ATerm)gsMakeProcEqnSpec(gstcWriteProcs(ATLgetArgument(ATAgetArgument(proc_spec,3),0))),3);
+  Result=ATsetArgument(Result,(ATerm)gsMakeProcessInit(ATAtableGet(body.proc_bodies,(ATerm)INIT_KEY())),4);
 
   Result=gstcFoldSortRefs(Result);
 
@@ -688,8 +693,10 @@ ATermAppl type_check_pbes_spec(ATermAppl pbes_spec)
 
 
   ATermAppl data_spec = ATAgetArgument(pbes_spec,0);
-  ATermAppl pb_eqn_spec = ATAgetArgument(pbes_spec,1);
-  ATermAppl pb_init = ATAgetArgument(pbes_spec,2);
+  ATermAppl glob_var_spec = ATAgetArgument(pbes_spec,1);
+  // XXX To do: implement type checking of global variable declaration
+  ATermAppl pb_eqn_spec = ATAgetArgument(pbes_spec,2);
+  ATermAppl pb_init = ATAgetArgument(pbes_spec,3);
 
   if(!gstcReadInSorts(ATLgetArgument(ATAgetArgument(data_spec,0),0))) {
     goto finally;
@@ -711,7 +718,7 @@ ATermAppl type_check_pbes_spec(ATermAppl pbes_spec)
 
   body.equations=ATLgetArgument(ATAgetArgument(data_spec,3),0);
 
-  if(!gstcReadInPBESAndInit(pb_eqn_spec,pb_init)) {
+  if(!gstcReadInPBESAndInit(glob_var_spec,pb_eqn_spec,pb_init)) {
     goto finally;
   }
   gsDebugMsg ("type checking PBES read-in phase finished\n");
@@ -728,11 +735,11 @@ ATermAppl type_check_pbes_spec(ATermAppl pbes_spec)
   data_spec=ATsetArgument(data_spec,(ATerm)gsMakeDataEqnSpec(body.equations),3);
   Result=ATsetArgument(pbes_spec,(ATerm)data_spec,0);
 
-  pb_eqn_spec=ATsetArgument(pb_eqn_spec,(ATerm)gstcWritePBES(ATLgetArgument(pb_eqn_spec,1)),1);
-  Result=ATsetArgument(Result,(ATerm)pb_eqn_spec,1);
+  pb_eqn_spec=ATsetArgument(pb_eqn_spec,(ATerm)gstcWritePBES(ATLgetArgument(pb_eqn_spec,0)),0);
+  Result=ATsetArgument(Result,(ATerm)pb_eqn_spec,2);
 
-  pb_init=ATsetArgument(pb_init,(ATerm)ATAtableGet(body.proc_bodies,(ATerm)INIT_KEY()),1);
-  Result=ATsetArgument(Result,(ATerm)pb_init,2);
+  pb_init=ATsetArgument(pb_init,(ATerm)ATAtableGet(body.proc_bodies,(ATerm)INIT_KEY()),0);
+  Result=ATsetArgument(Result,(ATerm)pb_init,3);
 
   Result=gstcFoldSortRefs(Result);
 
@@ -1320,14 +1327,14 @@ static ATbool gstcReadInProcsAndInit (ATermList Procs, ATermAppl Init){
   ATbool Result=ATtrue;
   for(;!ATisEmpty(Procs);Procs=ATgetNext(Procs)){
     ATermAppl Proc=ATAgetFirst(Procs);
-    ATermAppl ProcName=ATAgetArgument(ATAgetArgument(Proc,1),0);
+    ATermAppl ProcName=ATAgetArgument(ATAgetArgument(Proc,0),0);
 
     if(ATLtableGet(context.actions, (ATerm)ProcName)){
       gsErrorMsg("declaration of both process and action %P\n", ProcName);
       return ATfalse;
     }
 
-    ATermList ProcType=ATLgetArgument(ATAgetArgument(Proc,1),1);
+    ATermList ProcType=ATLgetArgument(ATAgetArgument(Proc,0),1);
 
     if(!gstcIsSortExprListDeclared(ProcType)) { return ATfalse; }
 
@@ -1350,11 +1357,11 @@ static ATbool gstcReadInProcsAndInit (ATermList Procs, ATermAppl Init){
     ATtablePut(context.processes,(ATerm)ProcName,(ATerm)Types);
 
     //check that all formal parameters of the process are unique.
-    ATermList ProcVars=ATLgetArgument(Proc,2);
+    ATermList ProcVars=ATLgetArgument(Proc,1);
     if(!gstcVarsUnique(ProcVars)){ gsErrorMsg("the formal variables in process %P are not unique\n",Proc); return ATfalse;}
 
-    ATtablePut(body.proc_pars,(ATerm)ATAgetArgument(Proc,1),(ATerm)ATLgetArgument(Proc,2));
-    ATtablePut(body.proc_bodies,(ATerm)ATAgetArgument(Proc,1),(ATerm)ATAgetArgument(Proc,3));
+    ATtablePut(body.proc_pars,(ATerm)ATAgetArgument(Proc,0),(ATerm)ATLgetArgument(Proc,1));
+    ATtablePut(body.proc_bodies,(ATerm)ATAgetArgument(Proc,0),(ATerm)ATAgetArgument(Proc,2));
     gsDebugMsg("Read-in Proc Name %T, Types %T\n",ProcName,Types);
   }
   ATtablePut(body.proc_pars,(ATerm)INIT_KEY(),(ATerm)ATmakeList0());
@@ -1363,11 +1370,11 @@ static ATbool gstcReadInProcsAndInit (ATermList Procs, ATermAppl Init){
   return Result;
 }
 
-static ATbool gstcReadInPBESAndInit(ATermAppl PBEqnSpec, ATermAppl PBInit){
+static ATbool gstcReadInPBESAndInit(ATermAppl GlobVarSpec, ATermAppl PBEqnSpec, ATermAppl PBInit){
   ATbool Result=ATtrue;
 
-  ATermList PBFreeVars=ATLgetArgument(PBEqnSpec,0);
-  ATermList PBEqns=ATLgetArgument(PBEqnSpec,1);
+  ATermList PBFreeVars=ATLgetArgument(GlobVarSpec,0);
+  ATermList PBEqns=ATLgetArgument(PBEqnSpec,0);
 
   for(;!ATisEmpty(PBEqns);PBEqns=ATgetNext(PBEqns)){
     ATermAppl PBEqn=ATAgetFirst(PBEqns);
@@ -1404,7 +1411,7 @@ static ATbool gstcReadInPBESAndInit(ATermAppl PBEqnSpec, ATermAppl PBInit){
     ATtablePut(context.PBs,(ATerm)PBName,(ATerm)Types);
 
     //check that all formal parameters of the PBES are unique.
-    if(!gstcVarsUnique(ATconcat(PBVars,PBFreeVars))){ gsErrorMsg("the formal and/or free variables in PBES %P are not unique\n",PBEqn); return ATfalse;}
+    if(!gstcVarsUnique(ATconcat(PBVars,PBFreeVars))){ gsErrorMsg("the formal and/or global variables in PBES %P are not unique\n",PBEqn); return ATfalse;}
 
     //This is a fake ProcVarId (There is no PBVarId)
     ATermAppl Index=gsMakeProcVarId(PBName,PBType);
@@ -1413,9 +1420,9 @@ static ATbool gstcReadInPBESAndInit(ATermAppl PBEqnSpec, ATermAppl PBInit){
     ATtablePut(body.proc_bodies,(ATerm)Index,(ATerm)ATAgetArgument(PBEqn,2));
     gsDebugMsg("Read-in Proc Name %T, Types %T\n",PBName,Types);
   }
-  ATtablePut(body.proc_freevars,(ATerm)INIT_KEY(),(ATerm)ATLgetArgument(PBInit,0));
+  ATtablePut(body.proc_freevars,(ATerm)INIT_KEY(),(ATerm)PBFreeVars);
   ATtablePut(body.proc_pars,(ATerm)INIT_KEY(),(ATerm)ATmakeList0());
-  ATtablePut(body.proc_bodies,(ATerm)INIT_KEY(),(ATerm)ATAgetArgument(PBInit,1));
+  ATtablePut(body.proc_bodies,(ATerm)INIT_KEY(),(ATerm)ATAgetArgument(PBInit,0));
 
   return Result;
 }
@@ -1425,8 +1432,7 @@ static ATermList gstcWriteProcs(ATermList oldprocs){
   for(ATermList ProcVars=oldprocs;!ATisEmpty(ProcVars);ProcVars=ATgetNext(ProcVars)){
     ATermAppl ProcVar=ATAgetArgument(ATAgetFirst(ProcVars),1);
     if(ProcVar==INIT_KEY()) continue;
-    Result=ATinsert(Result,(ATerm)gsMakeProcEqn(ATmakeList0(),
-                                                ProcVar,
+    Result=ATinsert(Result,(ATerm)gsMakeProcEqn(ProcVar,
 						ATLtableGet(body.proc_pars,(ATerm)ProcVar),
 						ATAtableGet(body.proc_bodies,(ATerm)ProcVar)
 						)

libraries/core/include/mcrl2/core/detail/constructors.h
@@ -45,7 +45,7 @@ ATermList constructList()
 
 //--- begin generated code
 ATermAppl constructBooleanOr();
-ATermAppl constructProcEqn();
+ATermAppl constructStateOr();
 ATermAppl constructHide();
 ATermAppl constructSortArrow();
 ATermAppl constructProcessAssignment();
@@ -154,7 +154,7 @@ ATermAppl constructPBESTrue();
 ATermAppl constructMultActName();
 ATermAppl constructIfThenElse();
 ATermAppl constructNil();
-ATermAppl constructStateOr();
+ATermAppl constructProcEqn();
 ATermAppl constructStructProj();
 ATermAppl constructPBEqn();
 ATermAppl constructWhr();
@@ -173,6 +173,7 @@ ATermAppl constructSortBag();
 ATermAppl constructPBInit();
 ATermAppl constructActTrue();
 ATermAppl constructRegTransOrNil();
+ATermAppl constructGlobVarSpec();
 ATermAppl constructActSpec();
 ATermAppl constructId();
 ATermAppl constructSortExpr();
@@ -211,19 +212,19 @@ ATermAppl constructBooleanOr()
   return t;
 }
 
-// ProcEqn
+// StateOr
 inline
-ATermAppl initConstructProcEqn(ATermAppl& t)
+ATermAppl initConstructStateOr(ATermAppl& t)
 {
-  t = ATmakeAppl4(gsAFunProcEqn(), reinterpret_cast<ATerm>(constructList()), reinterpret_cast<ATerm>(constructProcVarId()), reinterpret_cast<ATerm>(constructList()), reinterpret_cast<ATerm>(constructProcExpr()));
+  t = ATmakeAppl2(gsAFunStateOr(), reinterpret_cast<ATerm>(constructStateFrm()), reinterpret_cast<ATerm>(constructStateFrm()));
   ATprotect(reinterpret_cast<ATerm*>(&t));
   return t;
 }
 
 inline
-ATermAppl constructProcEqn()
+ATermAppl constructStateOr()
 {
-  static ATermAppl t = initConstructProcEqn(t);
+  static ATermAppl t = initConstructStateOr(t);
   return t;
 }
 
@@ -775,7 +776,7 @@ ATermAppl constructProcVarId()
 inline
 ATermAppl initConstructProcessInit(ATermAppl& t)
 {
-  t = ATmakeAppl2(gsAFunProcessInit(), reinterpret_cast<ATerm>(constructList()), reinterpret_cast<ATerm>(constructProcExpr()));
+  t = ATmakeAppl1(gsAFunProcessInit(), reinterpret_cast<ATerm>(constructProcExpr()));
   ATprotect(reinterpret_cast<ATerm*>(&t));
   return t;
 }
@@ -855,7 +856,7 @@ ATermAppl constructBooleanAnd()
 inline
 ATermAppl initConstructLinProcSpec(ATermAppl& t)
 {
-  t = ATmakeAppl4(gsAFunLinProcSpec(), reinterpret_cast<ATerm>(constructDataSpec()), reinterpret_cast<ATerm>(constructActSpec()), reinterpret_cast<ATerm>(constructLinearProcess()), reinterpret_cast<ATerm>(constructLinearProcessInit()));
+  t = ATmakeAppl5(gsAFunLinProcSpec(), reinterpret_cast<ATerm>(constructDataSpec()), reinterpret_cast<ATerm>(constructActSpec()), reinterpret_cast<ATerm>(constructGlobVarSpec()), reinterpret_cast<ATerm>(constructLinearProcess()), reinterpret_cast<ATerm>(constructLinearProcessInit()));
   ATprotect(reinterpret_cast<ATerm*>(&t));
   return t;
 }
@@ -903,7 +904,7 @@ ATermAppl constructChoice()
 inline
 ATermAppl initConstructLinearProcessInit(ATermAppl& t)
 {
-  t = ATmakeAppl2(gsAFunLinearProcessInit(), reinterpret_cast<ATerm>(constructList()), reinterpret_cast<ATerm>(constructList()));
+  t = ATmakeAppl1(gsAFunLinearProcessInit(), reinterpret_cast<ATerm>(constructList()));
   ATprotect(reinterpret_cast<ATerm*>(&t));
   return t;
 }
@@ -1063,7 +1064,7 @@ ATermAppl constructMu()
 inline
 ATermAppl initConstructPBEqnSpec(ATermAppl& t)
 {
-  t = ATmakeAppl2(gsAFunPBEqnSpec(), reinterpret_cast<ATerm>(constructList()), reinterpret_cast<ATerm>(constructList()));
+  t = ATmakeAppl1(gsAFunPBEqnSpec(), reinterpret_cast<ATerm>(constructList()));
   ATprotect(reinterpret_cast<ATerm*>(&t));
   return t;
 }
@@ -1191,7 +1192,7 @@ ATermAppl constructActExists()
 inline
 ATermAppl initConstructProcSpec(ATermAppl& t)
 {
-  t = ATmakeAppl4(gsAFunProcSpec(), reinterpret_cast<ATerm>(constructDataSpec()), reinterpret_cast<ATerm>(constructActSpec()), reinterpret_cast<ATerm>(constructProcEqnSpec()), reinterpret_cast<ATerm>(constructProcInit()));
+  t = ATmakeAppl5(gsAFunProcSpec(), reinterpret_cast<ATerm>(constructDataSpec()), reinterpret_cast<ATerm>(constructActSpec()), reinterpret_cast<ATerm>(constructGlobVarSpec()), reinterpret_cast<ATerm>(constructProcEqnSpec()), reinterpret_cast<ATerm>(constructProcInit()));
   ATprotect(reinterpret_cast<ATerm*>(&t));
   return t;
 }
@@ -1735,7 +1736,7 @@ ATermAppl constructActionRenameSpec()
 inline
 ATermAppl initConstructPBES(ATermAppl& t)
 {
-  t = ATmakeAppl3(gsAFunPBES(), reinterpret_cast<ATerm>(constructDataSpec()), reinterpret_cast<ATerm>(constructPBEqnSpec()), reinterpret_cast<ATerm>(constructPBInit()));
+  t = ATmakeAppl4(gsAFunPBES(), reinterpret_cast<ATerm>(constructDataSpec()), reinterpret_cast<ATerm>(constructGlobVarSpec()), reinterpret_cast<ATerm>(constructPBEqnSpec()), reinterpret_cast<ATerm>(constructPBInit()));
   ATprotect(reinterpret_cast<ATerm*>(&t));
   return t;
 }
@@ -1783,7 +1784,7 @@ ATermAppl constructActionRenameRule()
 inline
 ATermAppl initConstructLinearProcess(ATermAppl& t)
 {
-  t = ATmakeAppl3(gsAFunLinearProcess(), reinterpret_cast<ATerm>(constructList()), reinterpret_cast<ATerm>(constructList()), reinterpret_cast<ATerm>(constructList()));
+  t = ATmakeAppl2(gsAFunLinearProcess(), reinterpret_cast<ATerm>(constructList()), reinterpret_cast<ATerm>(constructList()));
   ATprotect(reinterpret_cast<ATerm*>(&t));
   return t;
 }
@@ -1955,19 +1956,19 @@ ATermAppl constructNil()
   return t;
 }
 
-// StateOr
+// ProcEqn
 inline
-ATermAppl initConstructStateOr(ATermAppl& t)
+ATermAppl initConstructProcEqn(ATermAppl& t)
 {
-  t = ATmakeAppl2(gsAFunStateOr(), reinterpret_cast<ATerm>(constructStateFrm()), reinterpret_cast<ATerm>(constructStateFrm()));
+  t = ATmakeAppl3(gsAFunProcEqn(), reinterpret_cast<ATerm>(constructProcVarId()), reinterpret_cast<ATerm>(constructList()), reinterpret_cast<ATerm>(constructProcExpr()));
   ATprotect(reinterpret_cast<ATerm*>(&t));
   return t;
 }
 
 inline
-ATermAppl constructStateOr()
+ATermAppl constructProcEqn()
 {
-  static ATermAppl t = initConstructStateOr(t);
+  static ATermAppl t = initConstructProcEqn(t);
   return t;
 }
 
@@ -2215,7 +2216,7 @@ ATermAppl constructSortBag()
 inline
 ATermAppl initConstructPBInit(ATermAppl& t)
 {
-  t = ATmakeAppl2(gsAFunPBInit(), reinterpret_cast<ATerm>(constructList()), reinterpret_cast<ATerm>(constructPropVarInst()));
+  t = ATmakeAppl1(gsAFunPBInit(), reinterpret_cast<ATerm>(constructPropVarInst()));
   ATprotect(reinterpret_cast<ATerm*>(&t));
   return t;
 }
@@ -2256,6 +2257,22 @@ inline
 ATermAppl constructRegTransOrNil()
 {
   static ATermAppl t = initConstructRegTransOrNil(t);
+  return t;
+}
+
+// GlobVarSpec
+inline
+ATermAppl initConstructGlobVarSpec(ATermAppl& t)
+{
+  t = ATmakeAppl1(gsAFunGlobVarSpec(), reinterpret_cast<ATerm>(constructList()));
+  ATprotect(reinterpret_cast<ATerm*>(&t));
+  return t;
+}
+
+inline
+ATermAppl constructGlobVarSpec()
+{
+  static ATermAppl t = initConstructGlobVarSpec(t);
   return t;
 }
 

libraries/core/include/mcrl2/core/detail/mcrl2parser.h
@@ -1,22 +1,23 @@
-
-/* A Bison parser, made by GNU Bison 2.4.1.  */
+/* A Bison parser, made by GNU Bison 2.3.  */
 
 /* Skeleton interface for Bison GLR parsers in C
-   
-      Copyright (C) 2002, 2003, 2004, 2005, 2006 Free Software Foundation, Inc.
-   
-   This program is free software: you can redistribute it and/or modify
+
+   Copyright (C) 2002, 2003, 2004, 2005, 2006 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-   
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -27,10 +28,9 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
-
 
 /* Tokens.  */
 #ifndef YYTOKENTYPE
@@ -93,64 +93,124 @@
      KWVAR = 310,
      KWEQN = 311,
      KWACT = 312,
-     KWPROC = 313,
-     KWPBES = 314,
-     KWINIT = 315,
-     KWSTRUCT = 316,
-     BOOL = 317,
-     POS = 318,
-     NAT = 319,
-     INT = 320,
-     REAL = 321,
-     LIST = 322,
-     SET = 323,
-     BAG = 324,
-     CTRUE = 325,
-     CFALSE = 326,
-     DIV = 327,
-     MOD = 328,
-     IN = 329,
-     LAMBDA = 330,
-     FORALL = 331,
-     EXISTS = 332,
-     WHR = 333,
-     END = 334,
-     DELTA = 335,
-     TAU = 336,
-     SUM = 337,
-     BLOCK = 338,
-     ALLOW = 339,
-     HIDE = 340,
-     RENAME = 341,
-     COMM = 342,
-     VAL = 343,
-     MU = 344,
-     NU = 345,
-     DELAY = 346,
-     YALED = 347,
-     NIL = 348,
-     ID = 349,
-     NUMBER = 350
+     KWGLOB = 313,
+     KWPROC = 314,
+     KWPBES = 315,
+     KWINIT = 316,
+     KWSTRUCT = 317,
+     BOOL = 318,
+     POS = 319,
+     NAT = 320,
+     INT = 321,
+     REAL = 322,
+     LIST = 323,
+     SET = 324,
+     BAG = 325,
+     CTRUE = 326,
+     CFALSE = 327,
+     DIV = 328,
+     MOD = 329,
+     IN = 330,
+     LAMBDA = 331,
+     FORALL = 332,
+     EXISTS = 333,
+     WHR = 334,
+     END = 335,
+     DELTA = 336,
+     TAU = 337,
+     SUM = 338,
+     BLOCK = 339,
+     ALLOW = 340,
+     HIDE = 341,
+     RENAME = 342,
+     COMM = 343,
+     VAL = 344,
+     MU = 345,
+     NU = 346,
+     DELAY = 347,
+     YALED = 348,
+     NIL = 349,
+     ID = 350,
+     NUMBER = 351
    };
 #endif
 
 
-#ifndef YYSTYPE
-typedef union YYSTYPE
-{
+/* Copy the first part of user declarations.  */
+#line 18 "mcrl2parser.yy"
 
-/* Line 2638 of glr.c  */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <aterm2.h>
+#include "mcrl2/core/aterm_ext.h"
+#include "mcrl2/core/messaging.h"
+#include "mcrl2/core/detail/struct.h"
+
+using namespace mcrl2::core;
+using namespace mcrl2::core::detail;
+
+//Global precondition: the ATerm library has been initialised
+
+//external declarations from mcrl2lexer.ll
+void mcrl2yyerror(const char *s);
+int mcrl2yylex(void);
+extern ATerm mcrl2_spec_tree;
+extern ATermIndexedSet mcrl2_parser_protect_table;
+
+#ifdef _MSC_VER
+#define yyfalse 0
+#define yytrue 1
+#endif
+
+#define YYMAXDEPTH 640000
+
+//local declarations
+ATermAppl gsDataSpecEltsToSpec(ATermList SpecElts);
+//Pre: SpecElts contains zero or more occurrences of sort, constructor,
+//     operation and data equation specifications.
+//Ret: data specification containing one sort, constructor, operation,
+//     and data equation specification, in that order.
+
+ATermAppl gsProcSpecEltsToSpec(ATermList SpecElts);
+//Pre: SpecElts contains one process initialisation and zero or more
+//     occurrences of sort, constructor, operation, data equation, action and
+//     process equation specifications.
+//Ret: process specification containing one sort, constructor, operation,
+//     data equation, action and process equation specification, and one
+//     process initialisation, in that order.
+
+ATermAppl gsActionRenameEltsToActionRename(ATermList SpecElts);
+//Pre: ActionRenameElts contains zero or more occurrences of
+//     sort, constructor, operation, equation, action and action rename
+//     rules.
+//Ret: specification containing one sort, constructor, operation, equation,
+//     action and action rename rules in that order.
+
+ATermAppl gsPBESSpecEltsToSpec(ATermList SpecElts);
+//Pre: SpecElts contains one parameterised boolean initialisation and zero or
+//     more occurrences of sort, constructor, operation, data equation, action
+//     and parameterised boolean equation specifications.
+//Ret: BPES specification containing one sort, constructor, operation,
+//     data equation, action and parameterised boolean equation specification,
+//     and one parameterised boolean initialisation, in that order.
+
+#define safe_assign(lhs, rhs) { ATbool b; lhs = rhs; ATindexedSetPut(mcrl2_parser_protect_table, (ATerm) lhs, &b); }
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE 
 #line 79 "mcrl2parser.yy"
-
+{
   ATerm term;
   ATermAppl appl;
   ATermList list;
-
-
-
-/* Line 2638 of glr.c  */
-#line 153 "../include/mcrl2/core/detail/mcrl2parser.h"
-} YYSTYPE;
+}
+/* Line 2604 of glr.c.  */
+#line 212 "../include/mcrl2/core/detail/mcrl2parser.h"
+	YYSTYPE;
+# define YYSTYPE_IS_DECLARED 1
 # define YYSTYPE_IS_TRIVIAL 1
 #endif
 
@@ -164,7 +224,6 @@ typedef struct YYLTYPE
 # define YYLTYPE_IS_DECLARED 1
 # define YYLTYPE_IS_TRIVIAL 1
 #endif
-
 
 
 extern YYSTYPE mcrl2yylval;

commit 5545c44d1406786722e3d3ab37f09e7ea4e5abf1

- fixed off by one error (introduced in revision r6602)
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@6607 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/core/source/typecheck.cpp
@@ -1429,8 +1429,8 @@ static ATbool gstcReadInPBESAndInit(ATermAppl GlobVarSpec, ATermAppl PBEqnSpec, 
 
 static ATermList gstcWriteProcs(ATermList oldprocs){
   ATermList Result=ATmakeList0();
-  for(ATermList ProcVars=oldprocs;!ATisEmpty(ProcVars);ProcVars=ATgetNext(ProcVars)){
-    ATermAppl ProcVar=ATAgetArgument(ATAgetFirst(ProcVars),1);
+  for(ATermList l=oldprocs;!ATisEmpty(l);l=ATgetNext(l)){
+    ATermAppl ProcVar=ATAgetArgument(ATAgetFirst(l),0);
     if(ProcVar==INIT_KEY()) continue;
     Result=ATinsert(Result,(ATerm)gsMakeProcEqn(ProcVar,
 						ATLtableGet(body.proc_pars,(ATerm)ProcVar),