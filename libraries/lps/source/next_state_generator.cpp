gijskant/mcrl2-pmc

commit d2e0b0191f81b1d0981fd587db82cbadee6450e6

Fix nextstate generator check on conditions.
The new implementation of the nextstate generator failed to check whether the solutions provided by the nextstate generator are exact. This means that the nextstate generator would enable transitions with non-false guards, instead of transitions for which the guard was true.

Now if the solution is not exact, we fail with an mcrl2::runtime_error.

Fixes #1073 of which #1075 was a duplicate.

git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@11122 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/lps/source/next_state_generator.cpp
@@ -470,6 +470,11 @@ void next_state_generator::iterator::increment()
   }
   else
   {
+    // If we failed to exactly rewrite the condition to true, nextstate generation fails.
+    if(!m_enumeration_iterator.solution_is_exact())
+    {
+      throw mcrl2::runtime_error("Expression " + data::pp(m_generator->m_rewriter.convert_from(m_summand->condition)) + " does not rewrite to true");
+    }
     valuation = *m_enumeration_iterator++;
   }
 

libraries/lps/test/next_state_generator_test.cpp
@@ -133,6 +133,28 @@ BOOST_AUTO_TEST_CASE(test_abp)
   }
 }
 
+BOOST_AUTO_TEST_CASE(test_non_true_condition)
+{
+  std::string text(
+    "map  b: Bool;\n"
+    "act  a;\n"
+    "proc P(s3: Pos) =\n"
+    "       (s3 == 1 && b) ->\n"
+    "         a .\n"
+    "         P(s3 = 2)\n"
+    "     + delta;\n"
+    "init P(1);\n"
+  );
+  specification spec = parse_linear_process_specification(text);
+  for (size_t i = 0; i < 8; i++)
+  {
+    // The respective sizes do not matter here, since we should get an exception in the nextstate
+    // generator.
+    BOOST_CHECK_THROW(test_next_state_generator(spec, 0, 0, 0, i&1, i&2, i&4), mcrl2::runtime_error);
+  }
+
+}
+
 boost::unit_test::test_suite* init_unit_test_suite(int argc, char* argv[])
 {
   MCRL2_ATERMPP_INIT(argc, argv)

commit 6f9a544686bb9a1150a448a8ce59a0b8ab9d295e

[release] Improve the error message introduced in r11122.
git-svn-id: https://svn.win.tue.nl/repos/MCRL2/trunk@11129 21252fe5-60e8-0310-8ff0-c1ae1001594e

libraries/lps/source/next_state_generator.cpp
@@ -473,7 +473,10 @@ void next_state_generator::iterator::increment()
     // If we failed to exactly rewrite the condition to true, nextstate generation fails.
     if(!m_enumeration_iterator.solution_is_exact())
     {
-      throw mcrl2::runtime_error("Expression " + data::pp(m_generator->m_rewriter.convert_from(m_summand->condition)) + " does not rewrite to true");
+      rewriter_expression_t reduced_condition(m_generator->m_rewriter.rewrite_internal(m_summand->condition, *m_substitution));
+      throw mcrl2::runtime_error("Expression " + data::pp(m_generator->m_rewriter.convert_from(m_summand->condition))
+                                 + " rewrites to " + data::pp(m_generator->m_rewriter.convert_from(reduced_condition))
+                                 + " instead of true");
     }
     valuation = *m_enumeration_iterator++;
   }