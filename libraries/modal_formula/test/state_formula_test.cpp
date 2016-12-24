4e8e05210950ace1fcdb51c8dabfb25e7f8e7f86
Gijs Kant <gijs@thehyve.nl>
2016-12-24 17:58:13 +0100

    Fix failing tests.

--- libraries/modal_formula/test/state_formula_test.cpp
+++ libraries/modal_formula/test/state_formula_test.cpp
@@ -374,6 +374,7 @@ BOOST_AUTO_TEST_CASE(preprocess_nested_modal_operators_test)
   test_preprocess_nested_modal_operators("[a]<b>true", lpsspec, "[a]mu X. <b>true");
   test_preprocess_nested_modal_operators("true && <a><a>true", lpsspec, "true && <a>mu X. <a>true");
   test_preprocess_nested_modal_operators("true => mu X. [a]<b>true", lpsspec, "true => mu X. [a]mu X1. <b>true");
+}
 
 BOOST_AUTO_TEST_CASE(test_vector)
 {
